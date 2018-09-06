f2c8d7e241f559f25adacba7ed76f907da950cea
Mike Cousins <mike@mikecousins.com>
2018-09-05 22:04:50 -0600

    Fixing classNames

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -14,10 +14,10 @@ class DashboardPage extends React.Component {
         <div className="mb-4 text-xl font-bold">
           Portfolio
         </div>
-        <button class="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded mb-4 mr-4">
+        <button className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded mb-4 mr-4">
           Balance
         </button>
-        <button class="bg-transparent hover:bg-green text-green-dark font-semibold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded" onClick={() => this.setState({ edit: !this.state.edit })}>
+        <button className="bg-transparent hover:bg-green text-green-dark font-semibold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded" onClick={() => this.setState({ edit: !this.state.edit })}>
           { !edit ? <span>Edit Groups</span> : <span>Stop Editing</span> }
         </button>
         <AccountGroup
