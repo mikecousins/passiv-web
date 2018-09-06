eded50cc74d5184d5335b7bad1f50db5df5a4eb8
Mike Cousins <mike@mikecousins.com>
2018-09-05 21:19:00 -0600

    Adding redux-devtools support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import './index.css';
 import App from './App';
@@ -16,7 +17,7 @@ const defaultState = {
   }
 };
 
-const store = createStore(rootReducer, defaultState, applyMiddleware(reduxThunk, logger));
+const store = createStore(rootReducer, defaultState, composeWithDevTools(applyMiddleware(reduxThunk, logger)));
 
 ReactDOM.render(
   <Provider store={store}>
