1c2a2bf36a27a64e12e7e83256a1b1aabd9139e0
Mike Cousins <mike@mikecousins.com>
2018-09-09 17:11:27 -0300

    Making login page redirect after login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { loginStartedAsync } from '../actions';
+import { selectLoggedIn } from '../selectors';
 
-const LoginPage = (props) => (
-  <React.Fragment>
-    <div>Login</div>
-    <div>{props.loggedIn ? <span>Logged In</span> : <span>Not Logged In</span>}</div>
-    <button onClick={props.startLogin} className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded mb-4 mr-4">Login</button>
-  </React.Fragment>
-);
+const LoginPage = (props) => {
+  if (props.loggedIn) {
+    return <Redirect to={props.location.state.nextPathname} />;
+  } else {
+    return (
+      <div>
+        <div>Login</div>
+        <div>{props.loggedIn ? <span>Logged In</span> : <span>Not Logged In</span>}</div>
+        <button onClick={props.startLogin} className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded mb-4 mr-4">Login</button>
+      </div>
+    );
+  }
+}
 
 const select = state => ({
-  loggedIn: state.auth.loggedIn,
+  loggedIn: selectLoggedIn(state),
 });
 
 const actions = { startLogin: loginStartedAsync };
