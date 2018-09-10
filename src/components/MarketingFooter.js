c2caed75c5ea87f8afb03f9f78086ddc3dbc7fea
Mike Cousins <mike@mikecousins.com>
2018-09-10 12:49:46 -0300

    Adding about page content and link

--- src/components/MarketingFooter.js
+++ src/components/MarketingFooter.js
@@ -7,7 +7,7 @@ const MarketingFooter = () => (
       <Link className="text-white hover:text-blue-darker no-underline" to="/blog">Blog</Link>
     </li>
     <li className="mr-6">
-      <Link className="text-white hover:text-blue-darker no-underline" to="/termsofuse">Terms Of Use</Link>
+      <Link className="text-white hover:text-blue-darker no-underline" to="/termsofservice">Terms Of Service</Link>
     </li>
     <li className="mr-6">
       <Link className="text-white hover:text-blue-darker no-underline" to="/privacy">Privacy</Link>
--- src/components/MarketingHeader.js
+++ src/components/MarketingHeader.js
@@ -6,6 +6,7 @@ const MarketingHeader = () => (
     <div className="flex items-center flex-no-shrink text-black mr-6">
       <Link to="/" className="font-semibold text-5xl tracking-tight no-underline">Passiv</Link>
       <Link to="/pricing" className="no-underline">Pricing</Link>
+      <Link to="/about" className="no-underline">About</Link>
       <Link to="/login" className="no-underline">Login</Link>
       <Link to="/register" className="no-underline">Try For Free</Link>
     </div>
