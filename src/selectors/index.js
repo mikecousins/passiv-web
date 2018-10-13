61bd22396d601b90ce3bc299408defc21bb33c6e
Mike Cousins <mike@mikecousins.com>
2018-10-13 15:32:21 -0600

    Balances are displaying now

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -4,6 +4,6 @@ export const selectSettings = state => state.settings;
 
 export const selectAccounts = state => state.accounts;
 
-export const selectBalances = state => state.balances;
+export const selectBalances = state => state.accountBalances;
 
-export const selectPositions = state => state.positions;
+export const selectPositions = state => state.accountPositions;
