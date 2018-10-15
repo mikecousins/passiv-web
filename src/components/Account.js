20200ee3b0f43abce712014b0887f3f97718797d
Mike Cousins <mike@mikecousins.com>
2018-10-14 21:38:25 -0600

    Displaying positions now

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,9 +6,24 @@ const Account = (props) => {
     <div className="flex py-2">
       <div className="w-1/4 bg-white py-1 align-middle">{props.account.meta.type} {props.account.number}</div>
       <div className="w-3/4 bg-white py-1 align-middle">
-        <label>
-          {props.balances && props.balances.data && props.balances.data.map(balance => <span>{balance.currency.name}: {balance.cash}</span>)}
-        </label>
+        <div>
+          {props.balances
+            && props.balances.data
+            && props.balances.data.map(balance => (
+              <span>{balance.currency.name}: {new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(balance.cash)}&nbsp;</span>
+            ))
+          }
+        </div>
+        <div>
+          {props.positions
+            && props.positions.data
+            && props.positions.data.map(position => (
+              <div key={position.id} >
+                <span title={position.symbol.description}>{position.symbol.symbol.symbol}</span> - {position.units} - {new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(position.price * position.units)}
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
