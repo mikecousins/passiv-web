09d730e365b5c7d7384ffd29c37c55c3a023face
Mike Cousins <mike@mikecousins.com>
2018-10-16 17:04:51 -0600

    Styling account metadata

--- src/components/AccountMetadata.js
+++ src/components/AccountMetadata.js
@@ -3,15 +3,41 @@ import PropTypes from 'prop-types';
 
 const AccountMetadata = (props) => (
   <div className="rounded overflow-hidden shadow-lg px-6 py-4 bg-white">
-    Account Name<br />
-    {props.name}<br />
-    {props.type}<br />
-    {props.number}<br />
-    {props.accurancy}%<br />
-    ${props.cash}<br />
-    ${props.equity}<br />
-    ${props.excludedEquity}<br />
-    ${props.cash + props.equity + props.excludedEquity}<br />
+    <div className="border-grey border-0 border-b-2 py-2 flex w-full">
+      <div className="w-2/3 text-4xl font-bold">
+        **Account Name**
+      </div>
+      <div className="w-1/6 text-center">
+        <h3>Type</h3>
+        {props.type}
+      </div>
+      <div className="w-1/6 text-center">
+        <h3>Account #</h3>
+        {props.number}
+      </div>
+    </div>
+    <div className="py-2 flex w-full">
+      <div className="w-1/5 border-grey border-0 border-r-2 px-2 text-center">
+        <h3>Accuracy</h3>
+        {props.accuracy}%
+      </div>
+      <div className="w-1/5 px-2 text-center">
+        <h3>Cash</h3>
+        {new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(props.cash)}
+      </div>
+      <div className="w-1/5 px-2 text-center">
+        <h3>Equity</h3>
+        {new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(props.equity)}
+      </div>
+      <div className="w-1/5 px-2 text-center">
+        <h3>Excluded</h3>
+        {new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(props.excludedEquity)}
+      </div>
+      <div className="w-1/5 px-2 text-center">
+        <h3>Total Value</h3>
+        <b>{new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(props.cash + props.equity + props.excludedEquity)}</b>
+      </div>
+    </div>
   </div>
 );
 
