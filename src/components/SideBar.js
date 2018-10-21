963dde760dc591db59966a27d1d8621dac6b8f9f
Mike Cousins <mike@mikecousins.com>
2018-10-20 19:51:10 -0600

    Adding toggle to demo mode

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { selectAccounts } from '../selectors';
+import { faSpinner, faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import { selectAccounts, selectIsDemoMode } from '../selectors';
 import { toggleDemoMode } from '../actions';
 
 const SideBar = (props) => {
@@ -24,13 +24,21 @@ const SideBar = (props) => {
       <Link to="/dashboard" className="block text-white no-underline text-lg tracking-wide pl-10 py-2">Dashboard</Link>
       {accounts}
       <Link to="/settings" className="block text-white no-underline text-lg tracking-wide pl-10 py-2">Settings</Link>
-      <button onClick={props.toggleDemo} className="block text-white no-underline text-lg tracking-wide pl-10 py-2">Demo</button>
+      <button
+        onClick={props.toggleDemo}
+        className="block text-white no-underline text-lg tracking-wide pl-10 py-2 focus:outline-none"
+      >
+        Demo Mode&nbsp;
+        {props.demoMode && <FontAwesomeIcon icon={faToggleOn} />}
+        {!props.demoMode && <FontAwesomeIcon icon={faToggleOff} />}
+      </button>
     </div>
   );
 }
 
 const select = state => ({
   accounts: selectAccounts(state),
+  demoMode:  selectIsDemoMode(state),
 });
 
 const actions = { toggleDemo: toggleDemoMode };
