c497e649c1baa42596bba129fc1d124e76e39c3d
Mike Cousins <mike@mikecousins.com>
2018-11-12 23:14:48 -0700

    Reworking lots of selectors

--- src/pages/GroupPage.js
+++ src/pages/GroupPage.js
@@ -2,41 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { selectAccounts, selectBalances, selectPositions, selectFullGroups } from '../selectors';
+import { selectCurrentGroup, selectCurrentPositions, selectCurrentCash, selectCurrentBalancedEquity } from '../selectors';
 import AccountMetadata from '../components/AccountMetadata';
 import AccountTargets from '../components/AccountTargets';
 import AccountBalance from '../components/AccountBalance';
 import AccountHoldings from '../components/AccountHoldings';
 
 const GroupPage = (props) => {
-  if (!props.groups || props.groups.length === 0) {
+  const { group } = props;
+  if (!props.group) {
     return <FontAwesomeIcon icon={faSpinner} spin />;
   }
-  const group = props.groups.find(group => group.id = props.match.params.groupId);
-  if (!group) {
-    return <span>Account not found</span>;
-  }
+
   const name = group.name || 'No Name Provided';
   let type = null;
   let number = null;
-  let equity = 0;
   const accuracy = 0;
-  let cash = 0;
   let excludedEquity = 0;
 
   if (group.accounts && group.accounts[0]) {
     type = group.accounts[0].type;
     number = group.accounts[0].number;
-
-    // get our cash
-    if (props.balances && props.balances[group.accounts[0].id] && props.balances[group.accounts[0].id].data) {
-      props.balances[group.accounts[0].id].data.forEach(balance => cash += parseFloat(balance.cash));
-    }
-
-    // get our equity
-    if (props.positions && props.positions[group.accounts[0].id] && props.positions[group.accounts[0].id].data) {
-      props.positions[group.accounts[0].id].data.forEach(position => equity += (parseFloat(position.price) * position.units));
-    }
   }
   return (
     <React.Fragment>
@@ -47,8 +33,8 @@ const GroupPage = (props) => {
             type={type}
             number={number}
             accuracy={accuracy}
-            cash={cash}
-            equity={equity}
+            cash={props.cash}
+            equity={props.balancedEquity}
             excludedEquity={excludedEquity}
           />
         </div>
@@ -65,7 +51,7 @@ const GroupPage = (props) => {
 
       <div className="flex mb-4">
         <div className="w-full">
-          <AccountHoldings positions={props.positions.data} />
+          <AccountHoldings positions={props.positions} />
         </div>
       </div>
     </React.Fragment>
@@ -73,10 +59,10 @@ const GroupPage = (props) => {
 }
 
 const select = state => ({
-  groups: selectFullGroups(state),
-  accounts: selectAccounts(state),
-  balances: selectBalances(state),
-  positions: selectPositions(state),
+  group: selectCurrentGroup(state),
+  positions: selectCurrentPositions(state),
+  cash: selectCurrentCash(state),
+  balancedEquity: selectCurrentBalancedEquity(state),
 });
 
 export default connect(select)(GroupPage);
