66f4b055716d65bcaad7b9c6ea167b87a45ac681
Brendan Wood <wood@synchroverge.com>
2018-12-17 14:56:03 -0400

    Set the Passiv logo to link back to the dashboard.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { selectLoggedIn } from '../selectors';
+
 
 const Header = () => (
   <nav className="flex items-center justify-between flex-wrap py-6 bg-white">
     <div className="flex items-center flex-no-shrink text-black mr-6">
-      <span className="font-semibold text-5xl tracking-tight">Passiv</span>
+      <Link to="/" >
+        <span className="font-semibold text-5xl tracking-tight">Passiv</span>
+      </Link>
     </div>
   </nav>
 );
 
-export default Header;
+const select = state => ({
+  loggedIn: selectLoggedIn(state),
+});
+
+export default connect(select)(Header);
