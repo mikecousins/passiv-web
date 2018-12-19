778cf98411c511bacf71c6b8ee62fe2df912c295
Mike Cousins <mike@mikecousins.com>
2018-12-18 23:27:11 -0700

    Fixing header unit test

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Header } from './Header';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
+  shallow(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>,
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
