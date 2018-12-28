4b97056360f48cb98bde174df314efda037697bc
Mike Cousins <mike@mikecousins.com>
2018-12-27 18:53:49 -0700

    Fixing header test

--- src/components/Header/tests/Header.test.js
+++ src/components/Header/tests/Header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import { Header } from './Header';
+import { Header } from '..';
 import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
