63451cf9ea82055a520347b209c8a0500d786a1c
Mike Cousins <mike@mikecousins.com>
2018-12-27 18:47:50 -0700

    Moving Header into our new folder structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import LogoutButton from './LogoutButton';
-import RefreshButton from './RefreshButton';
+import LogoutButton from '../LogoutButton';
+import RefreshButton from '../RefreshButton';
 
 
 export const Header = () => (
