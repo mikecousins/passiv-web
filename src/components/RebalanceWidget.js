c14379ee0c4419494da3146d395fa89bd7ca18b3
Brendan Wood <wood@synchroverge.com>
2019-02-19 15:49:34 -0400

    Updated the rebalancing widget to work better and show an order summary after successful execution.

--- src/components/RebalanceWidget.js
+++ src/components/RebalanceWidget.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { baseUrl, loadGroup } from '../actions';
 import { getData, postData } from '../api';
+import { selectSymbols } from '../selectors';
 import { Button } from '../styled/Button';
 import styled from '@emotion/styled';
-import { H2, P, A } from '../styled/GlobalElements';
+import { H2, P, A, Table } from '../styled/GlobalElements';
 import Number from './Number';
 
 const SummaryContainer = styled.div`
@@ -62,7 +63,7 @@ export class RebalanceWidget extends Component {
         this.setState({ placingOrders: false, orderResults: response, error: null });
 
         // reload group data following a successful order
-        this.props.reloadGroup({ ids: [this.props.groupId] });
+        // this.props.reloadGroup({ ids: [this.props.groupId] });
       })
       .catch(error => {
         this.setState({ placingOrders: false, orderResults: null, error: error });
@@ -77,6 +78,18 @@ export class RebalanceWidget extends Component {
       orderResults: null,
       error: null,
     });
+  }
+
+  closeWidget = () => {
+    this.setState({
+      placingOrders: false,
+      validatingOrders: false,
+      orderSummary: null,
+      orderResults: null,
+      error: null,
+    });
+    // reload group data following a successful order
+    this.props.reloadGroup({ ids: [this.props.groupId] });
   }
 
   sumEstimatedCommissions = () => {
@@ -95,50 +108,91 @@ export class RebalanceWidget extends Component {
     );
     if (this.state.validatingOrders) {
       orderValidation = (
-        <p>
+        <div>
+        <P>
           Validating orders ...&nbsp;
           <FontAwesomeIcon icon={faSpinner} spin />
-        </p>
+        </P>
+        </div>
       );
     } else if (this.state.orderSummary || this.state.error) {
       orderValidation = (
-        this.state.orderSummary ? (
+        this.state.orderResults ? (
           <div>
-            <H2>Order Summary</H2>
+            <H2>Order Results</H2>
             <div>
-              <MetaHorizontal>
-                <span>Estimated commissions:</span> <Number value={this.sumEstimatedCommissions()} currency />
-              </MetaHorizontal>
-              <MetaHorizontal>
-                <span>Remaining cash:</span> <Number value={this.sumRemainingCash()} currency />
-              </MetaHorizontal>
+              <Table>
+                <div>Action</div>
+                <div>Symbol</div>
+                <div>Units</div>
+                <div>Status</div>
+              </Table>
+              {
+                this.state.orderResults.map(results => {
+                  return (
+                    <Table key={results.trade}>
+                      <div>{ results.action }</div>
+                      <div>{ results.universal_symbol.symbol }</div>
+                      <div>{ results.filled_units }</div>
+                      <div>{ results.state }</div>
+                    </Table>
+                  )
+                })
+              }
             </div>
-            <P>
-              <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">Exchange and ECN fees</A>, <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">SEC fees</A> and for ADRs <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">annual custody</A> fees may apply. Commissions may vary if your order is filled over multiple days. Borrow fees may apply if you hold a short investment overnight.
-            </P>
-              {this.state.placingOrders ? (
-                <div>
-                  <p>Placing orders ... <FontAwesomeIcon icon={faSpinner} spin /></p>
-                </div>
-              ) : (
-                <div>
-                  <ConfirmContainer>
-                    <Button onClick={() => {this.cancelOrders()}}>
-                      Cancel
-                    </Button>
-                    <Button onClick={() => {this.confirmOrders()}}>
-                      Confirm
-                    </Button>
-                  </ConfirmContainer>
-                </div>
-              )}
+            <div>
+              <ConfirmContainer>
+                <Button onClick={() => {this.closeWidget()}}>
+                  Okay!
+                </Button>
+              </ConfirmContainer>
+            </div>
           </div>
         ) : (
-          <div>
+          this.state.orderSummary ? (
             <div>
-              There is a problem with your orders: {this.state.error}
+              <H2>Order Summary</H2>
+              <P>
+                The trades listed above will be placed as market orders on Questrade.
+              </P>
+              <div>
+                <MetaHorizontal>
+                  <span>Estimated commissions:</span> <Number value={this.sumEstimatedCommissions()} currency />
+                </MetaHorizontal>
+                <MetaHorizontal>
+                  <span>Remaining cash:</span> <Number value={this.sumRemainingCash()} currency />
+                </MetaHorizontal>
+              </div>
+              <P>
+                Market orders may result in the price paid or received to be different from the last price quoted before the order was placed. <A href="https://questrade-support.secure.force.com/mylearning/view/h/Investing/Market+orders" target="_blank">Learn more</A>
+              </P>
+              <P>
+                <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">Exchange and ECN fees</A>, <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">SEC fees</A> and for ADRs <A href="https://www.questrade.com/pricing/self-directed-investing/fees#exchange-ecn-fees" target="_blank">annual custody</A> fees may apply. Commissions may vary if your order is filled over multiple days. Borrow fees may apply if you hold a short investment overnight.
+              </P>
+                {this.state.placingOrders ? (
+                  <div>
+                    <p>Placing orders ... <FontAwesomeIcon icon={faSpinner} spin /></p>
+                  </div>
+                ) : (
+                  <div>
+                    <ConfirmContainer>
+                      <Button onClick={() => {this.cancelOrders()}}>
+                        Cancel
+                      </Button>
+                      <Button onClick={() => {this.confirmOrders()}}>
+                        Confirm
+                      </Button>
+                    </ConfirmContainer>
+                  </div>
+                )}
             </div>
-          </div>
+          ) : (
+            <div>
+              <P>
+                There is a problem with your orders: {this.state.error}
+              </P>
+            </div>
+          )
         )
       );
     }
@@ -156,4 +210,8 @@ const actions = {
   reloadGroup: loadGroup,
 };
 
-export default connect(null, actions)(RebalanceWidget);
+const select = state => ({
+  symbols: selectSymbols(state),
+});
+
+export default connect(select, actions)(RebalanceWidget);
