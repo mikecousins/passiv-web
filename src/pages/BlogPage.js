02a4b2f93eedaea6be64c851af3267dc2f5ac5b2
Mike Cousins <mike@mikecousins.com>
2019-02-26 20:14:40 -0700

    Removing marketing pages/packages

--- src/pages/BlogPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const BlogPage = () => (
-  <div>Blog Page</div>
-);
-
-export default BlogPage;
--- src/pages/PricingPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PricingPage = () => (
-  <React.Fragment>
-    <h1>Pricing Page</h1>
-  </React.Fragment>
-);
-
-export default PricingPage;
