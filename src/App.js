855d0fe5babe3f0e31b3c1b9e39aea26e50e69c6
Mike Cousins <mike@mikecousins.com>
2019-02-26 21:29:21 -0700

    Switched Stripe to async loading using hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { StripeProvider } from 'react-stripe-elements';
 import Layout from './layouts/Layout';
@@ -19,26 +19,39 @@ const questradeOauthRedirect = () => {
   return (<Redirect to={newPath}/>)
 }
 
-const App = () => (
-  <Layout>
-    <StripeProvider apiKey="pk_test_UEivjUoJpfSDWq5i4xc64YNK">
-      <Switch>
-        <Route path="/" exact render={() => (<Redirect to="/app/login"/>)} />
-        <Route path="/app" exact render={() => (<Redirect to="/app/login"/>)} />
-        <Route path="/app/login" component={LoginPage} />
-        <Route path="/app/register" component={RegistrationPage} />
-        <Route path="/app/reset-password" component={ResetPasswordPage} />
-        <Route path="/app/reset-password-confirm/:token" component={ResetPasswordConfirmPage} />
-        <SecureRoute path="/app/dashboard" component={DashboardPage} />
-        <SecureRoute path="/app/group/:groupId" component={GroupPage} />
-        <SecureRoute path="/app/settings" component={SettingsPage} />
-        <SecureRoute path="/app/oauth/questrade" component={QuestradeOauthPage} />
-        <Route exact path="/oauth/questrade" render={() => questradeOauthRedirect()} />
-        <Route exact path="/oauth/questrade-trade" render={() => questradeOauthRedirect()} />
-        <UpdateNotification />
-      </Switch>
-    </StripeProvider>
-  </Layout>
-);
+const App = () => {
+  const [stripe, setStripe] = useState(null);
+  useEffect(() => {
+    if (window.Stripe) {
+      setStripe(window.Stripe('pk_test_UEivjUoJpfSDWq5i4xc64YNK'));
+    } else {
+      document.querySelector('#stripe-js').addEventListener('load', () => {
+        // Create Stripe instance once Stripe.js loads
+        setStripe(window.Stripe('pk_test_UEivjUoJpfSDWq5i4xc64YNK'));
+      });
+    }
+  }, []);
+  return (
+    <Layout>
+      <StripeProvider stripe={stripe}>
+        <Switch>
+          <Route path="/" exact render={() => (<Redirect to="/app/login"/>)} />
+          <Route path="/app" exact render={() => (<Redirect to="/app/login"/>)} />
+          <Route path="/app/login" component={LoginPage} />
+          <Route path="/app/register" component={RegistrationPage} />
+          <Route path="/app/reset-password" component={ResetPasswordPage} />
+          <Route path="/app/reset-password-confirm/:token" component={ResetPasswordConfirmPage} />
+          <SecureRoute path="/app/dashboard" component={DashboardPage} />
+          <SecureRoute path="/app/group/:groupId" component={GroupPage} />
+          <SecureRoute path="/app/settings" component={SettingsPage} />
+          <SecureRoute path="/app/oauth/questrade" component={QuestradeOauthPage} />
+          <Route exact path="/oauth/questrade" render={() => questradeOauthRedirect()} />
+          <Route exact path="/oauth/questrade-trade" render={() => questradeOauthRedirect()} />
+          <UpdateNotification />
+        </Switch>
+      </StripeProvider>
+    </Layout>
+  );
+}
 
 export default App;
