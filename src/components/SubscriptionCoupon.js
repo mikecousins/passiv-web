bfce06cd807276ca3685aaeee0c3d57d14fc86b0
Brendan Kam <brendan.kam@getpassiv.com>
2019-03-19 15:12:24 -0300

    Added SubscriptionCoupon component

---
+++ src/components/SubscriptionCoupon.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSubscriptions } from '../selectors';
+
+import { H3 } from '../styled/GlobalElements';
+
+
+class SubscriptionCoupon extends React.Component {
+  render() {
+    const { coupon } = this.props.subscriptions
+
+    console.log(this.props.subscriptions)
+    if ( coupon.percent_off ){
+      return(
+        <React.Fragment>
+        <H3>Active Coupon: { coupon.code }</H3>
+        <p>Subscribe now and save <strong>{ coupon.percent_off }% off</strong> your annual subscription</p>
+        { coupon.redeem_by? <p>Expires on: {coupon.redeem_by}</p>: null}
+        </React.Fragment>
+      )
+    } else if ( coupon.amount_off ) {
+      return(
+        <React.Fragment>
+        <H3>Active Coupon: { coupon.code }</H3>
+        <p>Subscribe now and save <strong>${ coupon.amount_off } off</strong> your annual subscription</p>
+        </React.Fragment>
+      )
+    }else{
+      return(
+        null
+      )
+    }
+  }
+}
+
+const select = state => ({
+  subscriptions: selectSubscriptions(state),
+});
+const actions = {}
+
+export default connect(select, actions)(SubscriptionCoupon)
