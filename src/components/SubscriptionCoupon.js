db057ed32adbbf523d3f0e241e37ac618b40948a
Brendan Kam <brendan.kam@getpassiv.com>
2019-03-20 13:36:31 -0300

    Added button for users to remove coupon, updated CouponPage to refresh subscriptions

--- src/components/SubscriptionCoupon.js
+++ src/components/SubscriptionCoupon.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { selectSubscriptions } from '../selectors';
+import { selectSubscriptions, selectSettings } from '../selectors';
+import { putData } from '../api';
+import { loadSettings, loadSubscriptions } from '../actions';
 
-import { H3 } from '../styled/GlobalElements';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { H3, Edit } from '../styled/GlobalElements';
 
 
 class SubscriptionCoupon extends React.Component {
+  removeCoupon() {
+    let urlParams = new URLSearchParams(window.location.search);
+    let coupon = urlParams.get('code');
+
+    let settings = Object.assign({}, this.props.settings);
+    settings.coupon = null;
+    this.setState({loading: true, code: coupon});
+
+    putData('/api/v1/settings/', settings)
+      .then(response => {
+        this.setState({loading: false, success: true});
+        this.props.refreshSettings();
+        this.props.refreshSubscriptions();
+      })
+      .catch(error => {
+        this.setState({loading: false, error: error.response.data});
+        this.props.refreshSettings();
+        this.props.refreshSubscriptions();
+      });
+  }
+
   render() {
     const { coupon } = this.props.subscriptions
 
     if ( coupon.percent_off ){
       return(
         <React.Fragment>
-        <H3>Active Coupon: { coupon.code }</H3>
+        <H3>
+          Active Coupon: { coupon.code }
+          <Edit onClick={() => this.removeCoupon()}><FontAwesomeIcon icon={faTimes}/> Remove</Edit>
+        </H3>
         <p>Subscribe now and save <strong>{ coupon.percent_off }% off</strong> your annual subscription</p>
         { coupon.redeem_by? <p>Expires on: {coupon.redeem_by}</p>: null}
         </React.Fragment>
@@ -20,7 +49,10 @@ class SubscriptionCoupon extends React.Component {
     } else if ( coupon.amount_off ) {
       return(
         <React.Fragment>
-        <H3>Active Coupon: { coupon.code }</H3>
+        <H3>
+          Active Coupon: { coupon.code }
+          <Edit onClick={() => this.removeCoupon()}><FontAwesomeIcon icon={faTimes}/> Remove </Edit>
+        </H3>
         <p>Subscribe now and save <strong>${ coupon.amount_off } off</strong> your annual subscription</p>
         </React.Fragment>
       )
@@ -33,8 +65,11 @@ class SubscriptionCoupon extends React.Component {
 }
 
 const select = state => ({
+  settings: selectSettings(state),
   subscriptions: selectSubscriptions(state),
 });
-const actions = {}
-
+const actions = {
+  refreshSettings: loadSettings,
+  refreshSubscriptions: loadSubscriptions,
+};
 export default connect(select, actions)(SubscriptionCoupon)
