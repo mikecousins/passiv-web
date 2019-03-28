928186423aed0155ee6cc2512866191f885ec156
Mike Cousins <mike@mikecousins.com>
2019-03-27 22:23:15 -0600

    Cleaning up account holdings

--- src/components/AccountHoldings.js
+++ src/components/AccountHoldings.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import {H2,Title} from '../styled/GlobalElements';
@@ -28,54 +27,46 @@ export const HoldingsTable = styled.table`
   }
 `;
 
-class AccountHoldings extends Component {
-
-  render() {
-    return (
-      <ShadowBox>
-        <H2>Current Portfolio</H2>
-        <br />
-        <HoldingsTable>
-          <thead>
-            <tr>
-              <th><Title>Symbol</Title></th>
-              <th><Title>Units</Title></th>
-              <th><Title>Price</Title></th>
-              <th><Title>Value</Title></th>
-              <th><Title>Exclude</Title></th>
+export const AccountHoldings = ({ positions }) => (
+  <ShadowBox>
+    <H2>Current Portfolio</H2>
+    <br />
+    <HoldingsTable>
+      <thead>
+        <tr>
+          <th><Title>Symbol</Title></th>
+          <th><Title>Units</Title></th>
+          <th><Title>Price</Title></th>
+          <th><Title>Value</Title></th>
+          <th><Title>Exclude</Title></th>
+        </tr>
+      </thead>
+      <tbody>
+        {!positions && <tr><td colSpan="4"><FontAwesomeIcon icon={faSpinner} spin /></td></tr>}
+        {positions
+          && positions.map(position => (
+            <tr key={position.symbol.id}>
+              <td>
+                <span title={position.symbol.description}>{position.symbol.symbol}</span>
+              </td>
+              <td>
+                {position.units}
+              </td>
+              <td>
+                <Number value={position.price} currency />
+              </td>
+              <td>
+                <Number value={position.price * position.units} currency />
+              </td>
+              <td>
+                <ExcludedAssetToggle symbolId={position.symbol.id} />
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {!this.props.positions && <tr><td colSpan="4"><FontAwesomeIcon icon={faSpinner} spin /></td></tr>}
-            {this.props.positions
-              && this.props.positions.map(position => (
-                <tr key={position.symbol.id}>
-                  <td>
-                    <span title={position.symbol.description}>{position.symbol.symbol}</span>
-                  </td>
-                  <td>
-                    {position.units}
-                  </td>
-                  <td>
-                    <Number value={position.price} currency />
-                  </td>
-                  <td>
-                    <Number value={position.price * position.units} currency />
-                  </td>
-                  <td>
-                    <ExcludedAssetToggle symbolId={position.symbol.id} />
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </HoldingsTable>
-      </ShadowBox>
-    );
-  }
-}
+          ))
+        }
+      </tbody>
+    </HoldingsTable>
+  </ShadowBox>
+);
 
-const actions = {};
-const select = state => ({});
-
-export default connect(select, actions)(AccountHoldings);
+export default AccountHoldings;
