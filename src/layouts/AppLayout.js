c578876e208af12b59259a79c94f7cb442672843
Mike Cousins <mike@mikecousins.com>
2019-03-27 22:57:04 -0600

    Scrolling to the top of the page on mobile when you change pages

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Header from '../components/Header';
 import SlideMenu from '../components/SlideMenu';
 import GlobalStyle from '../styled/global';
+import ScrollHelper from '../components/ScrollHelper';
 
 const Container = styled.div`
   display: flex;
@@ -20,7 +21,7 @@ const Main = styled.main`
   }
 `;
 
-const AppLayout = (props) => (
+export const AppLayout = (props) => (
   <div>
     <GlobalStyle />
     <Header />
@@ -30,6 +31,7 @@ const AppLayout = (props) => (
         {props.children}
       </Main>
     </Container>
+    <ScrollHelper />
   </div>
 );
 
