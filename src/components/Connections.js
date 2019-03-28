f377ccc6c3a8ed01ed047351e47d0ad8c0206d7e
Mike Cousins <mike@mikecousins.com>
2019-03-27 21:53:27 -0600

    Simplifying connections component

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectAuthorizations } from '../selectors';
-
 import ConnectionsAuthorization from './ConnectionsAuthorization';
 
-class Connections extends React.Component {
-
-  render() {
-
-    const { authorizations } = this.props
-
-    if (authorizations){
-      if (authorizations.length > 0){
-        return(
-          <React.Fragment>
-
-          {authorizations.map(authorization => (
-            <ConnectionsAuthorization key={authorization.id} authorization={authorization}/>
-          ))}
-          </React.Fragment>
-        )} else {
-          return(null)
-        }
-    }else {
-      return(null)
-    }
+export const Connections = ({ authorizations }) => {
+  if (!authorizations || authorizations.length === 0) {
+    return null;
   }
-}
+
+  return (
+    <React.Fragment>
+      {authorizations.map(authorization => (
+        <ConnectionsAuthorization key={authorization.id} authorization={authorization}/>
+      ))}
+    </React.Fragment>
+  );
+};
 
 const select = state => ({
   authorizations: selectAuthorizations(state),
 });
-const actions = {};
 
-export default connect(select, actions)(Connections);
+export default connect(select)(Connections);
