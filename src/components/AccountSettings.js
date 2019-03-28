53d7c521a11bb45d408e530c67c27e93b8eb7ab9
Mike Cousins <mike@mikecousins.com>
2019-03-27 22:24:22 -0600

    Cleaning up account settings

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import {H2} from '../styled/GlobalElements';
 import ShadowBox from '../styled/ShadowBox';
 import SettingsToggle from './SettingsToggle';
 
-class AccountSettings extends Component {
+export const AccountSettings = () => (
+  <ShadowBox>
+    <H2>Settings</H2>
+    <br />
+    <SettingsToggle
+      name="Buy only"
+      settingsId="buy_only"
+    />
+  </ShadowBox>
+);
 
-  render() {
-    return (
-      <ShadowBox>
-        <H2>Settings</H2>
-        <br />
-        <SettingsToggle
-          name="Buy only"
-          settingsId="buy_only"
-        />
-      </ShadowBox>
-    );
-  }
-}
-
-const actions = {};
-const select = state => ({});
-
-export default connect(select, actions)(AccountSettings);
+export default AccountSettings;
