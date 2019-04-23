0e03eedf7b025924559e4bd458aeb2a3e451b178
Mike Cousins <mike@mikecousins.com>
2019-04-22 21:45:31 -0600

    Adding groups to the picker

--- src/components/PortfolioGroupPicker.js
+++ src/components/PortfolioGroupPicker.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { selectGroupsRaw } from '../selectors';
+import { selectGroups } from '../selectors';
 
-class PortfolioGroupPicker extends React.Component {
-  render() {
-    const groups = this.props.groups.data;
-    const { account } = this.props;
-
-    return (
-      <React.Fragment>
-        <select>
-          <option value="null">----------------------</option>
-          {groups.map(group =>
-            console.log(group.id === account.portfolio_group),
-          )}
-        </select>
-      </React.Fragment>
-    );
-  }
-}
+const PortfolioGroupPicker = ({ groups, account }) => (
+  <React.Fragment>
+    <select>
+      {groups.map(group => (
+        <option value={group.id} key={group.id}>
+          {group.name}
+        </option>
+      ))}
+    </select>
+  </React.Fragment>
+);
 
 const select = state => ({
-  groups: selectGroupsRaw(state),
+  groups: selectGroups(state),
 });
 
 export default connect(select)(PortfolioGroupPicker);
