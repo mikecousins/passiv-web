56da21c2cd1863b51b7cc8d5a33839d052a02793
Brendan Kam <brendan.kam@getpassiv.com>
2019-05-06 13:28:15 -0300

    Added error exclusion to show portfolio group targets for certain erros. Allows user to manually remove a target

--- src/components/PortfolioGroupTargets.js
+++ src/components/PortfolioGroupTargets.js
@@ -135,11 +135,16 @@ export class PortfolioGroupTargets extends React.Component {
 
   render() {
     const { target, error } = this.props;
-
-    console.log('target', target);
-    console.log('error', error);
+    console.log(error);
+    const excludedErrorCodes = [2001];
+    let excludedError = null;
 
     if (error !== null) {
+      let errorCode = Number(error.code);
+      excludedError = excludedErrorCodes.includes(errorCode);
+    }
+
+    if (error !== null && !excludedError) {
       return (
         <ShadowBox>
           <H2>Target Portfolio</H2>
