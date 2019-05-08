d7683c633f0bfeaab3375863faecb4d50c43fa92
Mike Cousins <mike@mikecousins.com>
2019-05-07 23:27:47 -0600

    Converting more reactor code to Typescript

--- src/reactors/init-runloop.ts
+++ src/reactors/init-runloop.ts
@@ -1,12 +1,26 @@
-import debounce from 'lodash-es/debounce';
 import ms from 'milliseconds';
+import { OutputSelector } from 'reselect';
 import { appIdle } from './app-lifecycle';
 import runDataChecks from './run-data-checks';
 
-export default function RunLoop() {
-  const actionPairs = [];
+const debounce = (callback: any, wait: number) => {
+  let timeout: NodeJS.Timeout;
+  return (...args: any[]) => {
+    const next = () => callback(...args);
+    clearTimeout(timeout);
+    timeout = setTimeout(next, wait);
+  };
+};
 
-  const start = function(store, pairs) {
+type ActionPair = {
+  selector: OutputSelector<any, any, any>;
+  actionCreator: () => (dispatch: any) => void;
+};
+
+export default function RunLoop() {
+  const actionPairs: ActionPair[] = [];
+
+  const start = (store: any, pairs: ActionPair[]) => {
     pairs.forEach(effect => {
       actionPairs.push(effect);
     });
@@ -24,7 +38,7 @@ export default function RunLoop() {
     callback();
   };
 
-  const addEffects = function(effects) {
+  const addEffects = (effects: ActionPair[]) => {
     effects.forEach(effect => {
       actionPairs.push(effect);
     });
