ae02a95e1ee0cc778f440b8c5e616b9a486664cd
Brendan Wood <wood@synchroverge.com>
2019-05-16 13:56:44 -0300

    Created an Offline component for the header. Fixed offline header color issue.

---
+++ src/components/Header/Offline.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { format } from 'date-fns';
+import styled from '@emotion/styled';
+
+export const OfflineStyle = styled.div`
+  margin: 0 0 0 20px;
+  padding-top: 20px;
+  text-align: left;
+  font-weight: 700;
+  @media (max-width: 900px) {
+    padding-top: 10px;
+    padding-right: 20px;
+  }
+`;
+
+const Offline = () => (
+  <OfflineStyle>
+    You are in offline mode. Check your internet connection.
+  </OfflineStyle>
+);
+
+export default Offline;
