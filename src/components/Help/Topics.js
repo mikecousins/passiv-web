e6b4c0d8e4e5a54ac90c7e08943e22635dbee731
Brendan Wood <wood@synchroverge.com>
2019-05-27 16:40:18 -0300

    Added some error handling to help topics rendering.

--- src/components/Help/Topics.js
+++ src/components/Help/Topics.js
@@ -9,17 +9,20 @@ const Container = styled.div``;
 
 export class Topics extends React.Component {
   render() {
-    const questions = this.props.articles.map(article => (
-      <ShadowBox key={article.slug}>
-        <H3>{article.title}</H3>
-        <P>{article.description}</P>
-        <Button
-          onClick={() => this.props.push(`/app/help/topic/${article.slug}`)}
-        >
-          Read More
-        </Button>
-      </ShadowBox>
-    ));
+    let questions = null;
+    if (this.props.articles) {
+      questions = this.props.articles.map(article => (
+        <ShadowBox key={article.slug}>
+          <H3>{article.title}</H3>
+          <P>{article.description}</P>
+          <Button
+            onClick={() => this.props.push(`/app/help/topic/${article.slug}`)}
+          >
+            Read More
+          </Button>
+        </ShadowBox>
+      ));
+    }
     return (
       <Container>
         <H2 margin="40px 0 25px">Help Topics</H2>
