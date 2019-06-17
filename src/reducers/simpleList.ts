294f6e3e1122af36d82272fb9ebfb88ecfbca2b4
Mike Cousins <mike@mikecousins.com>
2019-06-16 21:45:30 -0600

    Revert "Revert "Starting work on refreshing in place""
    This reverts commit 3db463db0c6c2c5e4aeb2857eee691cca2ea537d.

--- src/reducers/simpleList.ts
+++ src/reducers/simpleList.ts
@@ -22,18 +22,18 @@ const simpleList = <T extends object>({
   // here we're returning our customized reducer
   return (state = initialData, action: any) => {
     if (action.type === START) {
-      return Object.assign({}, state, {
-        [action.id]: {
-          loading: true,
-        },
-      });
+      const newState: SimpleListState = JSON.parse(JSON.stringify(state));
+      newState[action.id].loading = true;
+      return newState;
     }
+
     if (action.type === SUCCESS) {
       // if successful we store our data
       // store the lastFetch timestamp
       // clear out any errors
       // and set loading to false
-      return Object.assign({}, state, {
+      return {
+        ...state,
         [action.id]: {
           data: action.payload.data,
           lastFetch: Date.now(),
@@ -41,20 +41,22 @@ const simpleList = <T extends object>({
           lastError: null,
           loading: false,
         },
-      });
+      };
     }
+
     if (action.type === ERROR) {
       // we still want to leave existing
       // data intact as well as "last fetch"
       // which would let us determine if the
       // data is stale or not
-      return Object.assign({}, state, {
+      return {
+        ...state,
         [action.id]: {
           lastError: Date.now(),
           error: action.error,
           loading: false,
         },
-      });
+      };
     }
 
     if (action.type === 'LOGOUT' && userData) {
