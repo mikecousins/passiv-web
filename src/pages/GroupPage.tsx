6b221e1256c2b0f09396f7d1f1e595afa31a1a76
Kirsten Dodd <kirsten.jvd@gmail.com>
2019-08-10 20:59:29 -0700

    Styling of group view sub nav

--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -6,13 +6,32 @@ import { selectCurrentGroupId } from '../selectors/groups';
 import { selectAccounts } from '../selectors/accounts';
 import AccountTab from '../components/AccountTab';
 import GroupSettingsTab from '../components/GroupSettingsTab';
+import styled from '@emotion/styled';
+
+export const SubNav = styled.div`
+  border-bottom: 1px solid #e5e5e5;
+  margin: 0px 0 30px;
+
+  a {
+    color: #2a2d34;
+    display: inline-block;
+    font-weight: 600;
+    padding: 10px 50px 17px;
+    text-decoration: none;
+    border-bottom: 3px solid transparent;
+
+    &:hover {
+      border-bottom: 3px solid #03a287;
+    }
+  }
+`;
 
 const GroupPage = () => {
   const groupId = useSelector(selectCurrentGroupId);
   const accounts = useSelector(selectAccounts);
   return (
     <React.Fragment>
-      <div>
+      <SubNav>
         <Link to={`/app/group/${groupId}`}>Overview</Link>
         {accounts &&
           accounts.map(account => {
@@ -29,7 +48,7 @@ const GroupPage = () => {
             return null;
           })}
         <Link to={`/app/group/${groupId}/settings`}>Settings</Link>
-      </div>
+      </SubNav>
       <Route path="/app/group/:groupId" exact component={OverviewTab} />
       <Route path="/app/group/:groupId/:accountId" component={AccountTab} />
       <Route path="/app/group/:groupId/settings" component={GroupSettingsTab} />
