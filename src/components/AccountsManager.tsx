8d51ab38e8f85ad036a3dda1d23ae865e2785a56
Mike Cousins <mike@mikecousins.com>
2019-09-01 22:06:37 -0600

    Rewriting AccountsManager into Typescript

---
+++ src/components/AccountsManager.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuthorizations } from '../selectors';
+import Accounts from './Accounts';
+import ShadowBox from '../styled/ShadowBox';
+import { H2 } from '../styled/GlobalElements';
+
+const AccountsManager = () => {
+  const authorizations = useSelector(selectAuthorizations);
+
+  if (!authorizations) {
+    return null;
+  } else if (authorizations.length === 0) {
+    return null;
+  }
+
+  return (
+    <ShadowBox>
+      <H2>Accounts</H2>
+      <Accounts />
+    </ShadowBox>
+  );
+};
+
+export default AccountsManager;
