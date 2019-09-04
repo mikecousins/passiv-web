5037e8f0891c560c9b89e408c0fd35a64df65140
Mike Cousins <mike@mikecousins.com>
2019-09-03 22:18:35 -0600

    Rewriting Questrade OAuth page into Typescript

--- src/pages/QuestradeOauthPage.tsx
+++ src/pages/QuestradeOauthPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
-import qs from 'qs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { postData } from '../api';
@@ -10,18 +9,19 @@ import ShadowBox from '../styled/ShadowBox';
 import { H1, P } from '../styled/GlobalElements';
 import { Button } from '../styled/Button';
 import { Step } from '../styled/SignupSteps';
-import { selectRouter } from '../selectors/router';
+import { selectQueryTokens } from '../selectors/router';
 import { push } from 'connected-react-router';
+import { Error } from '../types/groupInfo';
 
-const QuestradeOauthPage = ({ router, reloadAllState, push }) => {
+const QuestradeOauthPage = () => {
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<Error>();
   const [success, setSuccess] = useState(false);
 
+  const queryParams = useSelector(selectQueryTokens);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const queryParams = qs.parse(router.location.search, {
-      ignoreQueryPrefix: true,
-    });
     const token = queryParams.code;
 
     if (token === null) {
@@ -31,7 +31,7 @@ const QuestradeOauthPage = ({ router, reloadAllState, push }) => {
       setLoading(true);
       postData('/api/v1/brokerages/authComplete/', { token: token })
         .then(() => {
-          reloadAllState();
+          dispatch(initialLoad());
           setTimeout(() => {
             setLoading(false);
             setSuccess(true);
@@ -42,7 +42,7 @@ const QuestradeOauthPage = ({ router, reloadAllState, push }) => {
           setError(error.response.data);
         });
     }
-  }, [reloadAllState, router.location.search]);
+  }, [dispatch, queryParams]);
 
   // if we're done, redirect the user to the dashboard
   if (success) {
@@ -124,7 +124,9 @@ const QuestradeOauthPage = ({ router, reloadAllState, push }) => {
           <Step>Failed to establish connection :(</Step>
           <ShadowBox>
             {errorDisplay}
-            <Button onClick={() => push('/app/settings')}>Settings</Button>
+            <Button onClick={() => dispatch(push('/app/settings'))}>
+              Settings
+            </Button>
           </ShadowBox>
         </React.Fragment>
       )}
@@ -132,16 +134,4 @@ const QuestradeOauthPage = ({ router, reloadAllState, push }) => {
   );
 };
 
-const select = state => ({
-  router: selectRouter(state),
-});
-
-const actions = {
-  reloadAllState: initialLoad,
-  push: push,
-};
-
-export default connect(
-  select,
-  actions,
-)(QuestradeOauthPage);
+export default QuestradeOauthPage;
