062537b6b638c82c3e846960bdf176b8bb21c9d9
Mike Cousins <mike@mikecousins.com>
2019-09-05 12:04:10 -0600

    Rewriting SignupSteps into typescript

--- src/styled/SignupSteps.ts
+++ src/styled/SignupSteps.ts
@@ -11,7 +11,7 @@ export const StepButton = styled.button`
   opacity: 0.97;
   border-radius: 2px;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-  background-color: ${props =>
+  background-color: ${(props: any) =>
     props.blue ? 'var(--brand-blue)' : 'var(--white)'};
   font-size: 25px;
   font-weight: 700;
@@ -20,7 +20,7 @@ export const StepButton = styled.button`
   line-height: 1.17;
   letter-spacing: 1px;
   text-align: center;
-  color: ${props => (props.blue ? 'var(--white)' : '#2a2d34')};
+  color: ${(props: any) => (props.blue ? 'var(--white)' : '#2a2d34')};
   flex: 1;
   padding: 20px;
   max-width: 49%;
@@ -35,7 +35,7 @@ export const SmallStepButton = styled.button`
   opacity: 0.97;
   border-radius: 2px;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-  background-color: ${props =>
+  background-color: ${(props: any) =>
     props.blue ? 'var(--brand-blue)' : 'var(--white)'};
   font-size: 25px;
   font-weight: 700;
@@ -44,7 +44,7 @@ export const SmallStepButton = styled.button`
   line-height: 1.17;
   letter-spacing: 1px;
   text-align: center;
-  color: ${props => (props.blue ? 'var(--white)' : '#2a2d34')};
+  color: ${(props: any) => (props.blue ? 'var(--white)' : '#2a2d34')};
   flex: 1;
   padding: 20px;
   max-width: 49%;
