c4ac660813cc71ee0b0dfcb59ca17259c0dc47f3
JacobCM <jacob-cm@outlook.com>
2019-11-14 18:37:18 -0500

    removed more unused imports/variables

--- src/pages/PerformancePage.tsx
+++ src/pages/PerformancePage.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import styled from '@emotion/styled';
-import { PerformanceGroups } from '../components/Performance/PerformanceGroups';
 import Performance from '../components/Performance/Performance';
-import { selectIsDemo } from '../selectors';
-import { useSelector } from 'react-redux';
 
 const PerformancePage = () => {
-  const isDemo = useSelector(selectIsDemo);
   return (
     <React.Fragment>
       <Performance />
