0a66d92d76759369bb3602fe12a6d93281f574a1
Mike Cousins <mike@mikecousins.com>
2019-11-14 23:45:29 -0700

    Created an OauthRoutes component so that it's shared

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -5,21 +5,25 @@ import {
   selectShowInsecureApp,
   selectShowOnboardingApp,
   selectShowSecureApp,
+  selectIsAuthorized,
 } from '../selectors';
 import { useSelector } from 'react-redux';
 import SecureApp from './SecureApp';
 import InsecureApp from './InsecureApp';
 import OnboardingApp from './OnboardingApp';
 import CommonRoutes from './CommonRoutes';
+import OauthRoutes from './OauthRoutes';
 
 const App = () => {
   const showInsecureApp = useSelector(selectShowInsecureApp);
   const showOnboardingApp = useSelector(selectShowOnboardingApp);
   const showSecureApp = useSelector(selectShowSecureApp);
+  const isAuthorized = useSelector(selectIsAuthorized);
   return (
     <Layout>
       <React.Fragment>
         <CommonRoutes />
+        {isAuthorized && <OauthRoutes />}
         {showInsecureApp && <InsecureApp />}
         {showOnboardingApp && <OnboardingApp />}
         {showSecureApp && <SecureApp />}
