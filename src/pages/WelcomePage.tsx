e55845d95034128e9a215802368ac40a95196a4a
Brendan Wood <wood@synchroverge.com>
2019-11-20 16:40:03 -0400

    Made the onboarding email scale to device width for smaller screens. Updated the video to use a more recent version.

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -4,6 +4,11 @@ import { push } from 'connected-react-router';
 import ShadowBox from '../styled/ShadowBox';
 import { H1DarkStyle, H2DarkStyle, VerticalPadding } from '../styled/Setup';
 import { Button } from '../styled/Button';
+import styled from '@emotion/styled';
+
+const ScalingIFrame = styled.iframe`
+  width: 100%;
+`;
 
 const WelcomePage = () => {
   const dispatch = useDispatch();
@@ -12,15 +17,15 @@ const WelcomePage = () => {
       <H1DarkStyle>Welcome to Passiv!</H1DarkStyle>
       <H2DarkStyle>Here's a 3-minute video to help you get started</H2DarkStyle>
       <VerticalPadding>
-        <iframe
+        <ScalingIFrame
           title="Passiv Basics"
           width="575"
           height="320"
-          src="https://player.vimeo.com/video/360152330"
+          src="https://player.vimeo.com/video/365762313"
           frameBorder="0"
           allow="autoplay; fullscreen"
           allowFullScreen
-        ></iframe>
+        ></ScalingIFrame>
       </VerticalPadding>
     </React.Fragment>
   );
