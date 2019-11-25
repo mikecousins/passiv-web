dee746885ed17145fb73e95b789d1543405606a3
Mike Cousins <mike@mikecousins.com>
2019-11-25 16:47:34 -0700

    Fixing build

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -57,15 +57,12 @@ export const selectCurrencies = createSelector<
   AppState,
   SimpleState<Currency[]>,
   Currency[] | null
->(
-  selectCurrenciesRaw,
-  rawCurrencies => {
-    if (rawCurrencies.data) {
-      return rawCurrencies.data;
-    }
-    return null;
-  },
-);
+>(selectCurrenciesRaw, rawCurrencies => {
+  if (rawCurrencies.data) {
+    return rawCurrencies.data;
+  }
+  return null;
+});
 
 export const selectCurrenciesNeedData = createSelector<
   AppState,
@@ -90,23 +87,17 @@ export const selectCurrenciesNeedData = createSelector<
 
 export const selectSettingsRaw = (state: AppState) => state.settings;
 
-export const selectSettings = createSelector(
-  selectSettingsRaw,
-  rawSettings => {
-    if (rawSettings.data) {
-      return rawSettings.data;
-    }
-  },
-);
+export const selectSettings = createSelector(selectSettingsRaw, rawSettings => {
+  if (rawSettings.data) {
+    return rawSettings.data;
+  }
+});
 
-export const selectIsDemo = createSelector(
-  selectSettings,
-  settings => {
-    if (settings) {
-      return settings.demo;
-    }
-  },
-);
+export const selectIsDemo = createSelector(selectSettings, settings => {
+  if (settings) {
+    return settings.demo;
+  }
+});
 
 export const selectBrokerages = createSelector(
   selectBrokeragesRaw,
@@ -185,14 +176,11 @@ export const selectSettingsNeedData = createSelector(
 
 export const selectPlansRaw = (state: AppState) => state.plans;
 
-export const selectPlans = createSelector(
-  selectPlansRaw,
-  rawPlans => {
-    if (rawPlans.data) {
-      return rawPlans.data;
-    }
-  },
-);
+export const selectPlans = createSelector(selectPlansRaw, rawPlans => {
+  if (rawPlans.data) {
+    return rawPlans.data;
+  }
+});
 
 export const selectPlansNeedData = createSelector(
   selectLoggedIn,
@@ -241,37 +229,31 @@ export const selectCurrencyRatesNeedData = createSelector(
   },
 );
 
-export const selectPasswordResetToken = createSelector(
-  selectRouter,
-  router => {
-    let token = null;
-    if (
-      router &&
-      router.location &&
-      router.location.pathname &&
-      router.location.pathname.split('/').length === 4
-    ) {
-      token = router.location.pathname.split('/')[3];
-    }
-    return token;
-  },
-);
+export const selectPasswordResetToken = createSelector(selectRouter, router => {
+  let token = null;
+  if (
+    router &&
+    router.location &&
+    router.location.pathname &&
+    router.location.pathname.split('/').length === 4
+  ) {
+    token = router.location.pathname.split('/')[3];
+  }
+  return token;
+});
 
-export const selectHelpArticleSlug = createSelector(
-  selectRouter,
-  router => {
-    let slug = null;
-    if (
-      router &&
-      router.location &&
-      router.location.pathname &&
-      router.location.pathname.split('/').length === 5
-    ) {
-      slug = router.location.pathname.split('/')[4];
-    }
-    return slug;
-  },
-);
+export const selectHelpArticleSlug = createSelector(selectRouter, router => {
+  let slug = null;
+  if (
+    router &&
+    router.location &&
+    router.location.pathname &&
+    router.location.pathname.split('/').length === 5
+  ) {
+    slug = router.location.pathname.split('/')[4];
+  }
+  return slug;
+});
 
 export const selectHelpArticlesRaw = (state: AppState) => state.helpArticles;
 
@@ -333,7 +315,7 @@ export const selectShowSecureApp = createSelector(
     if (showInsecureApp || showOnboardingApp) {
       return false;
     }
-    return true;
+    return false;
   },
 );
 
@@ -351,15 +333,12 @@ export const selectOnboardingPage = createSelector(
   },
 );
 
-export const selectName = createSelector(
-  selectSettings,
-  settings => {
-    if (settings) {
-      return settings.name;
-    }
-    return null;
-  },
-);
+export const selectName = createSelector(selectSettings, settings => {
+  if (settings) {
+    return settings.name;
+  }
+  return null;
+});
 
 export const selectIsUpdateServiceWorker = (state: AppState) =>
   state.updateServiceWorker;
