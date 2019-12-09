6fdde43ebc783874f5b8ae47b8cfbb63cd8212da
Brendan Wood <wood@synchroverge.com>
2019-12-09 11:52:01 -0400

    Added support for deep linking with query params so that coupon codes and auth codes get passed through.

--- src/apps/InsecureApp.tsx
+++ src/apps/InsecureApp.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Redirect, Switch, useLocation } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import ResetPasswordPage from '../pages/ResetPasswordPage';
 import ResetPasswordConfirmPage from '../pages/ResetPasswordConfirmPage';
 import RegistrationPage from '../pages/RegistrationPage';
 import DemoLoginPage from '../pages/DemoLoginPage';
+import { selectQueryTokens } from '../selectors/router';
 
 // hack to make routing work on both prod and dev
 const prefixPath = (path: string) => {
@@ -13,6 +15,20 @@ const prefixPath = (path: string) => {
 
 const InsecureApp = () => {
   const location = useLocation();
+
+  // include query params in deep link redirect
+  const queryParams = useSelector(selectQueryTokens);
+  if (queryParams.next) {
+    delete queryParams.next;
+  }
+  let appendParams = '';
+  if (queryParams.length > 0) {
+    const pieces = Object.entries(queryParams).map(([k, v]) => {
+      return `${k}%3D${v}`;
+    });
+    appendParams = '%3F' + pieces.join('%26');
+  }
+
   return (
     <Switch>
       <Route path={prefixPath('/login')} component={LoginPage} />
@@ -27,7 +43,9 @@ const InsecureApp = () => {
         component={ResetPasswordConfirmPage}
       />
       <Route path="*">
-        <Redirect to={prefixPath(`/login?next=${location.pathname}`)} />
+        <Redirect
+          to={prefixPath(`/login?next=${location.pathname}${appendParams}`)}
+        />
       </Route>
     </Switch>
   );
