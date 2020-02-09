b4a6c80bb82fdfac3320b4ca62681d4f6b02d544
Mike Cousins <mike@mikecousins.com>
2020-02-09 11:50:53 -0700

    Copying stuff from the latest typescript create-react-app template

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,5 @@
-// react-testing-library renders your components to document.body,
-// this will ensure they're removed after each test.
-import { cleanup } from '@testing-library/react';
-// this adds jest-dom's custom assertions
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-
-afterEach(cleanup);
