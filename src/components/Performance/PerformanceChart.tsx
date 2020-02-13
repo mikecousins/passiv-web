ca1ba421b7975149ea933bc350505a21836ba3e3
jacobcm <jacob-cm@outlook.com>
2020-02-12 21:37:25 -0500

    fixed bug that would crash performance page occasionally

--- src/components/Performance/PerformanceChart.tsx
+++ src/components/Performance/PerformanceChart.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Chart } from 'react-charts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export const ChartBox = styled.div`
   position: relative;
@@ -16,12 +18,21 @@ type Props = {
   axes: any;
 };
 
-export const PerformanceStat = (props: Props) => {
-  return (
-    <ChartBox>
-      <Chart data={props.data} axes={props.axes} tooltip />
-    </ChartBox>
-  );
+export const PerformanceChart = (props: Props) => {
+  if (props.data[0].data !== undefined) {
+    console.log(props.data[0].data);
+    return (
+      <ChartBox>
+        <Chart data={props.data} axes={props.axes} tooltip />
+      </ChartBox>
+    );
+  } else {
+    return (
+      <ChartBox>
+        <FontAwesomeIcon icon={faSpinner} spin />
+      </ChartBox>
+    );
+  }
 };
 
-export default PerformanceStat;
+export default PerformanceChart;
