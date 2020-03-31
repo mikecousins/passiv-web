8f984b5cb7a13cb55c3b2a626cc13d17c9677fd8
Brendan Wood <wood@synchroverge.com>
2020-03-31 13:50:35 -0300

    Made contact form on help page publicly available.

--- src/pages/HelpPage.tsx
+++ src/pages/HelpPage.tsx
@@ -4,22 +4,17 @@ import { Table } from '../styled/GlobalElements';
 import Tutorial from '../components/Help/Tutorial';
 import ContactForm from '../components/Help/ContactForm';
 import HelpHeader from '../components/Help/HelpHeader';
-import { selectLoggedIn } from '../selectors';
-import { useSelector } from 'react-redux';
 
 const HelpPage = () => {
-  const loggedIn = useSelector(selectLoggedIn);
   return (
     <React.Fragment>
-      {loggedIn && (
-        <React.Fragment>
-          <HelpHeader />
-          <Table>
-            <ContactForm />
-            <Tutorial />
-          </Table>
-        </React.Fragment>
-      )}
+      <React.Fragment>
+        <HelpHeader />
+        <Table>
+          <ContactForm />
+          <Tutorial />
+        </Table>
+      </React.Fragment>
       <FAQ />
     </React.Fragment>
   );
