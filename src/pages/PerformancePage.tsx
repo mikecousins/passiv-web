a8ac7a659f9bb211660af9ce88164a54717a7022
jacobcm <jacob-cm@outlook.com>
2020-04-07 14:19:25 -0400

    added performance load to reactor effects with 1 day stale time, removed performance call from performance page

--- src/pages/PerformancePage.tsx
+++ src/pages/PerformancePage.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { loadPerformanceAll } from '../actions/performance';
+import React from 'react';
 import Performance from '../components/Performance/Performance';
 
 const PerformancePage = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadPerformanceAll());
-  }, [dispatch]);
-
   return <Performance />;
 };
 
