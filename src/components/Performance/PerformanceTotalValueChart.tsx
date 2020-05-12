0186be4ce99690474c8b55648dc0c5fa2115e18e
jacobcm <jacob-cm@outlook.com>
2020-05-12 17:41:22 -0400

    Fixed typo in excluded assets label

--- src/components/Performance/PerformanceTotalValueChart.tsx
+++ src/components/Performance/PerformanceTotalValueChart.tsx
@@ -45,7 +45,7 @@ export const PerformanceTotalValueChart = () => {
   let badTickerString = '';
   if (showBadTickers) {
     badTickerString =
-      'The following assets may have been exlcuded due to difficulties retrieving accurate price history: ' +
+      'The following assets may have been excluded due to difficulties retrieving accurate price history: ' +
       badTickers?.join(', ');
   }
 
