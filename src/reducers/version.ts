c43c4dff1200d6ad9a00227a12bc01a40207470b
Brendan Wood <wood@synchroverge.com>
2020-07-09 16:28:31 -0300

    Moved autoreload into a timeout function to make sure it gets run after the redux update.

--- src/reducers/version.ts
+++ src/reducers/version.ts
@@ -7,7 +7,9 @@ const version = (state = initialState, action: any) => {
     const newVersion = action.payload.version;
     if (state.version > 0 && state.version !== newVersion) {
       // outdated version, force app reload
-      window.location.reload();
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     }
     return { ...state, version: newVersion };
   }
