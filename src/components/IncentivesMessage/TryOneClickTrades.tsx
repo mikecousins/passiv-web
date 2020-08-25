49c05fa7c6b0fe18da651426698f25d8e34daa4c
Kirsten <kirsten.jvd@gmail.com>
2020-08-25 13:20:57 -0700

    update to styling of one click trades "trial"

--- src/components/IncentivesMessage/TryOneClickTrades.tsx
+++ src/components/IncentivesMessage/TryOneClickTrades.tsx
@@ -13,22 +13,42 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const RemainingBox = styled.div`
+const TryOneClickContainer = styled.div`
+  background: #fff;
+  border-left: 2px solid #023ca2;
+  background-color: white;
+  margin-bottom: -2px;
+  display: inline-block;
+`;
+
+const RemainingBox = styled.button`
   font-size: 17px;
-  font-weight: 1000;
-  background: white;
-  color: #003ba2;
   border: 1px;
   width: max-content;
   margin: 10px 0;
   border-radius: 10px;
   padding: 4px;
+  span {
+    margin: 0 7px;
+  }
+  svg {
+    color: #003ba2;
+    font-size: 20px;
+  }
+`;
+const LearnMore = styled.span`
+  color: #003ba2;
+  font-weight: 700;
+  font-size: 16px;
+  svg {
+    vertical-align: middle;
+    margin-bottom: 3px;
+  }
 `;
 
 const ExplanationBox = styled.div`
-  padding: 4px;
-  margin: 7px 0;
-  background: white;
+  padding: 5px 20px 20px 18px;
+  color: #232225;
 `;
 
 const OrderedList = styled.ol`
@@ -50,46 +70,53 @@ const TryOneClickTrades = () => {
   const toggleShowExplanation = () => {
     setShowExplanation(!showExplanation);
   };
-
   if (incentives && incentives.free_one_click_trade && isCommunityUser) {
     return (
       <React.Fragment>
-        <RemainingBox onClick={() => toggleShowExplanation()}>
-          <FontAwesomeIcon icon={faGift} />
-          &nbsp; Make a free One-Click Trade &nbsp;
-          {showExplanation ? (
-            <FontAwesomeIcon icon={faCaretUp} />
-          ) : (
-            <FontAwesomeIcon icon={faCaretDown} />
-          )}
-        </RemainingBox>
+        <div>
+          <TryOneClickContainer>
+            <RemainingBox onClick={() => toggleShowExplanation()}>
+              <FontAwesomeIcon icon={faGift} />
+              <span>
+                You have{' '}
+                <strong>
+                  {`${incentives.free_one_click_trade} free One-Click Trades`}
+                </strong>{' '}
+                to try!
+              </span>
+              {showExplanation ? (
+                <LearnMore>
+                  <FontAwesomeIcon icon={faCaretUp} />
+                </LearnMore>
+              ) : (
+                <LearnMore>
+                  Learn More <FontAwesomeIcon icon={faCaretDown} />
+                </LearnMore>
+              )}
+            </RemainingBox>
 
-        {showExplanation ? (
-          <ExplanationBox>
-            <P>
-              Our goal at Passiv is to make investing easier for you. Would you
-              like to try our <strong>One-Click Trade</strong> feature?
-            </P>
-            <P>Here's how you can get started:</P>
-            <OrderedList>
-              <li>
-                Click on <strong>Preview Orders</strong> to get an estimate for
-                your orders.
-              </li>
-              <li>
-                Place your orders by clicking on the <strong>Confirm</strong>{' '}
-                button.
-              </li>
-            </OrderedList>
-            <P>
-              You have{' '}
-              <strong>
-                {`${incentives.free_one_click_trade} free One-Click Trades Remaining`}
-                .
-              </strong>
-            </P>
-          </ExplanationBox>
-        ) : null}
+            {showExplanation ? (
+              <ExplanationBox>
+                <P>
+                  Our goal at Passiv is to make investing easier for you. Would
+                  you like to try our elite <strong>One-Click Trade</strong>{' '}
+                  feature?
+                </P>
+                <P>Here's how you can get started:</P>
+                <OrderedList>
+                  <li>
+                    Click on <strong>Preview Orders</strong> to get an estimate
+                    for your orders.
+                  </li>
+                  <li>
+                    Place your orders by clicking on the{' '}
+                    <strong>Confirm</strong> button.
+                  </li>
+                </OrderedList>
+              </ExplanationBox>
+            ) : null}
+          </TryOneClickContainer>
+        </div>
       </React.Fragment>
     );
   } else {
