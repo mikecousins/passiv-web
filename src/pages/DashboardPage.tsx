f954ec4c40733bbda47b4cf4b693b19ab7bc7729
Brendan Wood <wood@synchroverge.com>
2020-08-25 22:23:57 -0300

    Fixed compile warning.

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -9,8 +9,6 @@ import TotalHoldings from '../components/TotalHoldings';
 import QuestradeAuthorizationPicker from '../components/QuestradeAuthorizationPicker';
 import WelcomeVideo from '../components/WelcomeVideo/WelcomeVideo';
 
-import { TradesContainer } from '../styled/Group';
-
 export const DashboardPage = () => {
   const authorized = useSelector(selectIsAuthorized);
   const groups = useSelector(selectDashboardGroups);
