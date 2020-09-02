90fa69b4d31534d44b1a5f3be9a4465bb3b733c7
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-02 10:00:21 -0300

    set up referral code pulling

--- src/selectors/referrals.ts
+++ src/selectors/referrals.ts
@@ -3,7 +3,18 @@ import ms from 'milliseconds';
 import shouldUpdate from '../reactors/should-update';
 import { AppState } from '../store';
 
-export const selectPhoneNumber = createSelector(selectSettings, (settings) => {
+export const selectSettingsRaw = (state: AppState) => state.settings;
+
+export const selectSettings = createSelector(
+  selectSettingsRaw,
+  (rawSettings) => {
+    if (rawSettings.data) {
+      return rawSettings.data;
+    }
+  },
+);
+
+export const selectReferralCode = createSelector(selectSettings, (settings) => {
   if (settings) {
     return settings.referral_code;
   }
