a4f121233a7b236c5ced0c92500e94ceaf3bcea6
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-02 17:55:58 -0300

    imported selectSettings instead of re-defining it

--- src/selectors/referrals.ts
+++ src/selectors/referrals.ts
@@ -1,16 +1,6 @@
 import { createSelector } from 'reselect';
 import { AppState } from '../store';
-
-export const selectSettingsRaw = (state: AppState) => state.settings;
-
-export const selectSettings = createSelector(
-  selectSettingsRaw,
-  (rawSettings) => {
-    if (rawSettings.data) {
-      return rawSettings.data;
-    }
-  },
-);
+import { selectSettings } from './index';
 
 export const selectReferralCode = createSelector(selectSettings, (settings) => {
   if (settings) {
