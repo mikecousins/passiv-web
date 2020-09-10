e1d34fcc6e77592ca83d1b4378e99070e76859a2
Brendan Kam <brendankam1990@gmail.com>
2020-09-10 17:43:23 -0300

    Component to change order_target_by settings

--- src/components/PortfolioGroupSettings/OrderTargetAllocations.tsx
+++ src/components/PortfolioGroupSettings/OrderTargetAllocations.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { putData } from '../../api';
+import { loadGroup } from '../../actions';
+import { toast } from 'react-toastify';
 import styled from '@emotion/styled';
 import { Formik, Field } from 'formik';
 import { Form } from '../../styled/Form';
+import { H3, P } from '../../styled/GlobalElements';
 
 import {
   selectCurrentGroupSettings,
   selectCurrentGroupId,
 } from '../../selectors/groups';
+
+const InlineDiv = styled.div`
+  display: inline-block;
+  width: 50%;
+`;
 
 const StyledFieldBase = styled(Field)`
   width: 50%;
@@ -19,12 +27,12 @@ const StyledSelect = styled(StyledFieldBase)`
   display: inline-block;
   background-color: #fff;
   border-radius: 0;
-  border: 1px solid #000;
+  border: 1px solid #03846d;
   -webkit-appearance: none;
 
   background-image: linear-gradient(45deg, #0000 50%, #fff 50%),
     linear-gradient(135deg, #fff 50%, #0000 50%),
-    linear-gradient(to right, #2a2d34, #2a2d34);
+    linear-gradient(to right, #04a287, #04a287);
 
   background-position: calc(100% - 20px) calc(1em + 2px),
     calc(100% - 15px) calc(1em + 2px), 100% 0;
@@ -33,43 +41,69 @@ const StyledSelect = styled(StyledFieldBase)`
   background-repeat: no-repeat;
 `;
 
-const OrderTargetAllocations = () => {
+type Props = {
+  edit: boolean;
+};
+
+const OrderTargetAllocations = ({ edit }: Props) => {
   const settings = useSelector(selectCurrentGroupSettings);
+  const groupId = useSelector(selectCurrentGroupId);
   const dispatch = useDispatch();
 
   const orderByOptions = {
-    Alphabetical: 0,
-    'Target Allocation Percentage': 1,
-    'Actual Assets Percentage': 2,
-    'Most Underweight Assets': 3,
-    'Most Overweight Assets': 4,
+    Alphabetically: 0,
+    'By Target Allocation': 1,
+    'By Asset Percentages': 2,
+    'By Most Underweight': 3,
+    'By Most Overweight': 4,
   };
 
   const updateSettings = () => {
     if (settings) {
-      console.log('Hello');
+      putData(`/api/v1/portfolioGroups/${groupId}/settings/`, settings)
+        .then(() => {
+          dispatch(loadGroup({ ids: [groupId] }));
+        })
+        .catch(() => {
+          toast.error('Failed to update settings');
+        });
+    }
+  };
+
+  const handleChange = (e: any) => {
+    if (settings) {
+      settings.order_targets_by = e.target.value;
+      updateSettings();
     }
   };
 
   const orderByOptionsEntries = Object.entries(orderByOptions);
+
+  if (!edit) {
+    return null;
+  }
+
   return (
     <React.Fragment>
-      <Formik
-        initialValues={{
-          value: '3',
-        }}
-        onSubmit={() => console.log('Hello')}
-      >
-        <Form>
-          <StyledSelect as="select" name="orderBy">
-            {orderByOptionsEntries.map(entry => (
-              <option value={entry[1]} key={entry[0].split(' ').join('')}>
-                {entry[0]}
-              </option>
-            ))}
-          </StyledSelect>
-        </Form>
-      </Formik>
+      <InlineDiv>
+        <H3> Order Targets</H3>
+        <Formik
+          initialValues={{
+            orderBy: settings ? settings.order_targets_by : 0,
+          }}
+          onSubmit={() => {}}
+        >
+          <Form onChange={e => handleChange(e)}>
+            <StyledSelect as="select" name="orderBy">
+              {orderByOptionsEntries.map(entry => (
+                <option value={entry[1]} key={entry[0].split(' ').join('')}>
+                  {entry[0]}
+                </option>
+              ))}
+            </StyledSelect>
+          </Form>
+        </Formik>
+      </InlineDiv>
     </React.Fragment>
   );
 };
