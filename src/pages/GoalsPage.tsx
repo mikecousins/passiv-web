8d93762590c2b84c8471148f40a280cc8fe39181
jacobcm <jacob-cm@outlook.com>
2020-09-25 14:02:37 -0400

    removed goals feature flag check for goals page

--- src/pages/GoalsPage.tsx
+++ src/pages/GoalsPage.tsx
@@ -8,7 +8,11 @@ import Goals from '../components/Goals/Goals';
 const GoalsPage = () => {
   const goalsFeature = useSelector(selectGoalsFeature);
 
-  return <React.Fragment>{goalsFeature && <Goals />}</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Goals />
+    </React.Fragment>
+  );
 };
 
 export default GoalsPage;
