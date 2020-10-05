d6b82dd46588d12da4b649b83097811c5de1712b
Nick McCullum <nicholasmccullum@gmail.com>
2020-10-05 13:13:46 -0300

    got unstyled cash notifications working

--- src/components/SettingsManager/CashNotificationSettings.tsx
+++ src/components/SettingsManager/CashNotificationSettings.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { selectSettings } from '../../selectors';
 import { loadSettings } from '../../actions';
 import { putData } from '../../api';
 import { ToggleButton, StateText } from '../../styled/ToggleButton';
-import { OptionsTitle, DisabledBox } from '../../styled/GlobalElements';
+import {
+  Edit,
+  SubSetting,
+  OptionsTitle,
+  DisabledBox,
+} from '../../styled/GlobalElements';
+import { NumericTextInput } from '../../styled/Form';
+import { SmallButton } from '../../styled/Button';
+import Number from './../Number';
+import { Settings } from '../../types/settings';
+
+const calcDecimalPlaces = (number: number) => {
+  let decimalPlaces = 4;
+  const asString = String(number);
+  if (asString.indexOf('.') >= 0) {
+    const pieces = asString.split('.');
+    if (pieces.length === 2) {
+      decimalPlaces = pieces[1].length;
+    }
+  }
+  return decimalPlaces;
+};
 
 const CashNotificationSettings = () => {
   const settings = useSelector(selectSettings);
   const dispatch = useDispatch();
+  const [editingThreshold, setEditingThreshold] = useState(false);
+  const [cashThreshold, setCashThreshold] = useState();
 
   const updateNotification = () => {
     if (!settings) {
@@ -33,6 +56,28 @@ const CashNotificationSettings = () => {
     return null;
   }
 
+  const disabled = !settings.receive_cash_notifications;
+
+  const finishEditingThreshold = () => {
+    if (!settings) {
+      return;
+    }
+
+    let newSettings: Settings = { ...settings };
+
+    newSettings.cash_email_threshold = cashThreshold;
+
+    putData('/api/v1/settings/', newSettings)
+      .then(() => {
+        dispatch(loadSettings());
+      })
+      .catch(() => {
+        dispatch(loadSettings());
+      });
+
+    setEditingThreshold(false);
+  };
+
   return (
     <React.Fragment>
       <OptionsTitle>Cash Notifications:</OptionsTitle>
@@ -47,6 +92,51 @@ const CashNotificationSettings = () => {
             <FontAwesomeIcon icon={faToggleOff} />
             <StateText>off</StateText>
           </React.Fragment>
+        )}
+        {settings.receive_cash_notifications ? (
+          <React.Fragment>
+            <SubSetting>
+              <OptionsTitle>Cash Notification Threshold:</OptionsTitle>
+              {!editingThreshold ? (
+                <React.Fragment>
+                  <Number
+                    value={parseFloat(settings.cash_email_threshold)}
+                    percentage
+                    decimalPlaces={calcDecimalPlaces(cashThreshold)}
+                  />
+                  <Edit
+                    onClick={() => setEditingThreshold(true)}
+                    disabled={disabled}
+                  >
+                    <FontAwesomeIcon icon={faPen} />
+                    Edit
+                  </Edit>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <NumericTextInput
+                    value={cashThreshold}
+                    onChange={e => setCashThreshold(e.target.value)}
+                    onKeyPress={e => {
+                      if (e.key === 'Enter') {
+                        finishEditingThreshold();
+                      }
+                    }}
+                    disabled={disabled}
+                  />{' '}
+                  %
+                  <SmallButton
+                    onClick={finishEditingThreshold}
+                    disabled={disabled}
+                  >
+                    Done
+                  </SmallButton>
+                </React.Fragment>
+              )}
+            </SubSetting>
+          </React.Fragment>
+        ) : (
+          <React.Fragment></React.Fragment>
         )}
       </ToggleButton>
       <DisabledBox>
