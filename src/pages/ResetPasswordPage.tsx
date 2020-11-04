2e6c2f5b1486f8fd9c4d67841136ff8ef820aa48
Brendan Kam <brendankam1990@gmail.com>
2020-11-04 09:06:08 -0400

    Fix for password reset page not working

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -43,7 +43,7 @@ const ResetPasswordPage = ({ location }: Props) => {
               initialValues={{
                 email: '',
               }}
-              validate={values => {
+              validate={(values) => {
                 let errors: any = {};
                 if (!values.email) {
                   errors.email = 'An email is required.';
@@ -60,7 +60,7 @@ const ResetPasswordPage = ({ location }: Props) => {
                   .then(() => {
                     setSubmitted(true);
                   })
-                  .catch(error => {
+                  .catch((error) => {
                     let errors: any = {};
                     if (
                       error.response.data &&
@@ -75,8 +75,7 @@ const ResetPasswordPage = ({ location }: Props) => {
                     actions.setErrors(errors);
                   });
               }}
-            >
-              {({
+              render={({
                 touched,
                 errors,
                 values,
@@ -85,7 +84,7 @@ const ResetPasswordPage = ({ location }: Props) => {
                 handleSubmit,
                 isValid,
               }) => (
-                <Form>
+                <Form onSubmit={handleSubmit}>
                   <Label htmlFor="email">Email</Label>
                   <Input
                     onChange={handleChange}
@@ -107,7 +106,7 @@ const ResetPasswordPage = ({ location }: Props) => {
                   </div>
                 </Form>
               )}
-            </Formik>
+            />
           </div>
         )}
       </React.Fragment>
