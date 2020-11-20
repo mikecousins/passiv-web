afd1f8777fdb6c5ddb1bb927055f3708995c8e7d
Brendan Wood <wood@synchroverge.com>
2020-11-20 10:20:04 -0400

    Removed dependency array from effect on WealthicaConnectionUpdatePage to fix compile warnings.

--- src/pages/WealthicaConnectionUpdatePage.tsx
+++ src/pages/WealthicaConnectionUpdatePage.tsx
@@ -43,7 +43,7 @@ const WealthicaConnectionUpdatePage = ({ onboarding }: Props) => {
           setConnectionFail(true);
         });
     }
-  }, []);
+  });
 
   const handleSuccess = () => {
     dispatch(reloadEverything());
