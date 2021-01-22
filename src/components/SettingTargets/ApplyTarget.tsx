2e412517128aacea4ef4bd35a774599d7fde89f8
ShayanAbedi <abedi.shayan@unb.ca>
2021-01-22 14:16:18 -0400

    continue working on apply target page

--- src/components/SettingTargets/ApplyTarget.tsx
+++ src/components/SettingTargets/ApplyTarget.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getData, postData } from '../../api';
 import styled from '@emotion/styled';
-import { loadModelPortfolios } from '../../actions';
+import { loadModelPortfolios, loadGroup, loadGroupInfo } from '../../actions';
 import { selectModelPortfolios } from '../../selectors/modelPortfolios';
 import { ModelPortfolioDetailsType } from '../../types/modelPortfolio';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -19,23 +19,21 @@ import {
 import {
   selectCurrentGroup,
   selectCurrentGroupInfo,
-  selectCurrentGroupPositions,
+  selectCurrentGroupPositionsWithActualPercentage,
   selectCurrentGroupTarget,
-  selectCurrentGroupTotalEquityExcludedRemoved,
   selectCurrentGroupTrades,
-  selectPreferredCurrency,
 } from '../../selectors/groups';
 import ShadowBox from '../../styled/ShadowBox';
 import Grid from '../../styled/Grid';
-import { H2, H3, P, Table } from '../../styled/GlobalElements';
+import { ErrorBox, H2, H3, P, Table, H1 } from '../../styled/GlobalElements';
 import { GreyBox } from './SettingTargets';
 import { BackButton } from '../ModelPortfolio/ModelPortfolio';
 import { toast } from 'react-toastify';
-import AssetClassModelDetails from './AssetClassModelDetails';
 import { Button } from '../../styled/Button';
 import ApplySecurityModel from './ApplySecurityModel';
-import { debug } from 'console';
 import { selectCurrencyRates } from '../../selectors';
+import { selectCurrentGroupSettings } from '../../selectors/groups';
+import { ErrorAttributeSpan } from '../RebalanceWidget/styles';
 
 const StyledMenuButton = styled(MenuButton)`
   border: 1px solid var(--brand-blue);
@@ -67,6 +65,7 @@ const TopInfo = styled.div`
 
 const GroupName = styled(H2)`
   font-weight: 400;
+  font-size: 40px;
   line-height: 50px;
 `;
 const AssetDetails = styled.div`
@@ -106,6 +105,13 @@ const NoSecurities = styled(P)`
   font-weight: 600;
 `;
 
+const Alert = styled.div`
+  background: hsla(10, 50%, 50%, 0.2);
+  padding: 10px;
+  width: 50%;
+  font-size: 20px;
+`;
+
 const ApplyTarget = () => {
   const dispatch = useDispatch();
   const currentGroup = useSelector(selectCurrentGroup);
@@ -114,162 +120,81 @@ const ApplyTarget = () => {
   const modelPortfolios: ModelPortfolioDetailsType[] = useSelector(
     selectModelPortfolios,
   );
-  const [selectedName, setSelectedName] = useState('Apply a Model');
+  const [selectedModel, setSelectedModel] = useState<any>();
+  const [originalModel, setOriginalModel] = useState<any>(
+    currentGroupInfo?.model_portfolio,
+  );
   const [selectedModelDetails, setSelectedModelDetails] = useState<any>();
   const [showDetails, setShowDetails] = useState(false);
   const [showDetailsId, setShowDetailsId] = useState('');
-  const [modelChanged, setModelChanged] = useState(false);
   const [loading, setLoading] = useState(false);
   const currentGroupTarget = useSelector(selectCurrentGroupTarget);
   const trades = useSelector(selectCurrentGroupTrades);
-  const currentGroupPositions = useSelector(selectCurrentGroupPositions);
-  const preferredCurrency = useSelector(selectPreferredCurrency);
-  const totalHoldingsExcludedRemoved = useSelector(
-    selectCurrentGroupTotalEquityExcludedRemoved,
+  const [modelHasChanged, setModelHasChanged] = useState(
+    currentGroupInfo?.settings.model_portfolio_changed,
   );
-  const rates = useSelector(selectCurrencyRates);
+  const positions = useSelector(
+    selectCurrentGroupPositionsWithActualPercentage,
+  );
 
   const loadModelForCurrentGroup = (id: any) => {
-    // getData(
-    //   `/api/v1/portfolioGroups/${currentGroup?.id}/modelPortfolio/${currentGroupInfo?.model_portfolio?.id}`,
-    // )
-    //   .then((res) => {
-    //     setSelectedModelDetails(res.data);
-    //     setSelectedName(currentGroupInfo?.model_portfolio?.name);
-    //     setLoading(false);
-    //   })
-    //   .catch(() => {
-    //     toast.error('Loading Model Failed.');
-    //   });
+    // dispatch(loadGroupInfo());
     let model = modelPortfolios.find(
       (model: ModelPortfolioDetailsType) => model.model_portfolio.id === id,
     );
+    setSelectedModel(model?.model_portfolio);
+
     //@ts-ignore
     model?.model_portfolio_security.map((security: any) => {
-      currentGroupPositions?.map((position: any) => {
-        if (security.symbol.id === position.symbol.id) {
-          //@ts-ignore
-          if (
-            preferredCurrency &&
-            position.symbol.currency.id === preferredCurrency.id
-          ) {
-            security.actualPercentage =
-              ((position.price * position.units) /
-                totalHoldingsExcludedRemoved) *
-              100;
-          } else {
-            const conversionRate = rates?.find(
-              (rate: any) =>
-                preferredCurrency &&
-                rate.src.id === position.symbol.currency.id &&
-                rate.dst.id === preferredCurrency.id,
-            );
-            if (conversionRate) {
-              security.actualPercentage =
-                ((position.price * position.units) /
-                  totalHoldingsExcludedRemoved) *
-                100 *
-                conversionRate.exchange_rate;
-            }
-          }
-        }
-      });
+      const position = positions?.find(
+        (position: any) => security.symbol.id === position.symbol.id,
+      );
+      security.actualPercentage = position?.actualPercentage;
     });
     const securitiesNotInTarget = trades?.trades.filter(
       (trade: any) => !trade.symbol_in_target,
     );
     //@ts-ignore
     model.securities_not_in_targets = securitiesNotInTarget;
-    setSelectedModelDetails(model);
-    //@ts-ignore
-    setSelectedName(model?.model_portfolio.name);
+    const newArrayOfObj = model?.model_portfolio_security.map(
+      ({ symbol: fullSymbol, ...rest }) => ({
+        fullSymbol,
+        ...rest,
+      }),
+    );
+    setSelectedModelDetails(newArrayOfObj);
   };
 
   useEffect(() => {
-    if (currentGroupInfo?.model_portfolio.model_type === 0) {
-      setLoading(true);
+    // if (
+    //   currentGroupInfo?.model_portfolio?.model_type === 0 &&
+    //   modelHasChanged
+    // ) {
+    //   // setLoading(true);
+    //   // let model = currentGroupTarget;
+    //   // const newArrayOfObj = model?.map(({ fullSymbol: symbol, ...rest }) => ({
+    //   //   ...rest,
+    //   // }));
+    //   // console.log(newArrayOfObj);
+    //   setSelectedModelDetails(currentGroupTarget);
+    //   setSelectedModel(currentGroupInfo?.model_portfolio);
+    // } else if (
+    //   currentGroupInfo?.model_portfolio?.model_type === 0 &&
+    //   !modelHasChanged
+    // ) {
+    //   loadModelForCurrentGroup(currentGroupInfo?.model_portfolio.id);
+    // }
+    if (currentGroupInfo?.model_portfolio?.model_type === 0) {
       loadModelForCurrentGroup(currentGroupInfo?.model_portfolio.id);
     }
-    setLoading(false);
-  }, [currentGroup, currentGroupInfo]);
+    // setLoading(false);
+  }, []);
 
   const handleModelSelect = (model: ModelPortfolioDetailsType) => {
     if (model.model_portfolio.model_type === 0) {
       loadModelForCurrentGroup(model.model_portfolio.id);
       // setLoading(true);
-      // getData(
-      //   `/api/v1/portfolioGroups/${currentGroup?.id}/modelPortfolio/${model.model_portfolio.id}`,
-      // )
-      //   .then((res) => {
-      //     // dispatch(loadModelPortfolios());
-      //     // dispatch(loadGroupInfo());
-      //     // loadModelForCurrentGroup();
-      //     setSelectedModelDetails(res.data);
-      //     setSelectedName(model.model_portfolio.name);
-      //     setModelChanged(true);
-      //     setLoading(false);
-      //     toast.success(
-      //       `"${model.model_portfolio.name}" model applied to "${currentGroup?.name}".`,
-      //       { autoClose: 3000 },
-      //     );
-      //   })
-      //   .catch(() => {
-      //     toast.error(
-      //       `"${model.model_portfolio.name}" model failed to apply to "${currentGroup?.name}".`,
-      //       { autoClose: 3000 },
-      //     );
-      //   });
-      //
     }
-    // else {
-    //   const priorities: any[] = [];
-    //   model.model_portfolio_asset_class.map((assetClass) => {
-    //     let assetClassObj;
-    //     let assetClassSecurities = getAssetClass(
-    //       assetClass.model_asset_class.id,
-    //     );
-    //     let symbolPriority: any[] = [];
-    //     // @ts-ignore
-    //     assetClassSecurities.map((securityId: any) => {
-    //       symbolPriority.push({
-    //         symbol_id: securityId,
-    //         accounts_id: [],
-    //       });
-    //     });
-    //     assetClassObj = {
-    //       model_asset_class: {
-    //         id: assetClass.model_asset_class.id,
-    //         name: assetClass.model_asset_class.name,
-    //       },
-    //       symbols_accounts_priorities: symbolPriority,
-    //     };
-    //     priorities.push(assetClassObj);
-    //   });
-    //   const body = {
-    //     priorities: priorities,
-    //     securities_not_in_targets: [],
-    //     excluded_symbols: [],
-    //   };
-    //   postData(
-    //     `/api/v1/portfolioGroups/${currentGroup?.id}/modelPortfolio/${model.model_portfolio.id}`,
-    //     body,
-    //   )
-    //     .then((res) => {
-    //       dispatch(loadModelPortfolios());
-    //       dispatch(loadGroupInfo());
-    //       loadModelForCurrentGroup();
-    //       toast.success(
-    //         `"${model.model_portfolio.name}" model applied to "${currentGroup?.name}".`,
-    //         { autoClose: 3000 },
-    //       );
-    //     })
-    //     .catch(() => {
-    //       toast.error(
-    //         `"${model.model_portfolio.name}" model failed to apply to "${currentGroup?.name}".`,
-    //         { autoClose: 3000 },
-    //       );
-    //     });
-    // }
   };
 
   const handleNewModelBtn = () => {
@@ -289,8 +214,18 @@ const ApplyTarget = () => {
     return false;
   };
 
-  const handleSaveButton = () => {
-    setModelChanged(false);
+  const handleApplyModelBtn = () => {
+    postData(
+      `api/v1/portfolioGroups/${currentGroup?.id}/modelPortfolio/${selectedModel.id}`,
+      {},
+    ).then(() => {
+      dispatch(loadGroupInfo());
+      setOriginalModel(selectedModel);
+      toast.success(
+        `'${selectedModel.name}' has applied to '${currentGroup?.name}'`,
+        { autoClose: 3000 },
+      );
+    });
   };
 
   return (
@@ -301,6 +236,7 @@ const ApplyTarget = () => {
         </Link>
       </BackButton>
       <TopInfo>
+        {/* {modelHasChanged && <Alert> ❗️ The Model Has Changed.</Alert>} */}
         <Grid columns="1fr 300px">
           <GroupName>{currentGroup?.name}</GroupName>
           <Menu>
@@ -308,7 +244,7 @@ const ApplyTarget = () => {
               {loading ? (
                 <FontAwesomeIcon icon={faSpinner} spin />
               ) : (
-                selectedName
+                selectedModel?.name
               )}
               <span aria-hidden style={{ marginLeft: '30px' }}>
                 <FontAwesomeIcon icon={faAngleDown} size="lg" />
@@ -342,10 +278,13 @@ const ApplyTarget = () => {
       <div>
         {currentGroupInfo?.model_portfolio?.model_type === 0 &&
           selectedModelDetails && (
-            <ApplySecurityModel model={selectedModelDetails} />
+            <ApplySecurityModel
+              model={selectedModelDetails}
+              modelPortfolio={selectedModel}
+            />
           )}
       </div>
-      {/* {selectedModelDetails?.securities_not_in_targets.length > 0 && (
+      {selectedModelDetails?.securities_not_in_targets?.length > 0 && (
         <SecurityNotInTarget>
           <H3 style={{ marginBottom: '20px' }}>
             Other Securities in this Group
@@ -363,8 +302,10 @@ const ApplyTarget = () => {
             },
           )}
         </SecurityNotInTarget>
-      )} */}
-      {modelChanged && <Button onClick={() => handleSaveButton()}>Save</Button>}
+      )}
+      {originalModel.id !== selectedModel?.id && (
+        <Button onClick={() => handleApplyModelBtn()}>Apply this Model</Button>
+      )}
     </ShadowBox>
   );
 };
