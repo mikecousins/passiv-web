401a5d1ff6f3e87fb400feed467b856491d28c47
Nick McCullum <nicholasmccullum@gmail.com>
2021-01-29 13:44:04 -0400

    added proper Zerodha config to App.tsx

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -113,6 +113,10 @@ const TDAmeritradeOauthPage = ReactLazyPreload(() =>
   ),
 );
 
+const ZerodhaOauthPage = ReactLazyPreload(() =>
+  import(/* webpackChunkName: "zerodha-oauth" */ '../pages/ZerodhaOauthPage'),
+);
+
 const WealthicaOauthPage = ReactLazyPreload(() =>
   import(
     /* webpackChunkName: "td-ameritrade-oauth" */ '../pages/WealthicaOauthPage'
@@ -241,6 +245,13 @@ const interactiveBrokersOauthRedirect = () => {
 const tdAmeritradeOauthRedirect = () => {
   let urlParams = new URLSearchParams(window.location.search);
   let newPath = '/app/oauth/td?' + urlParams;
+  return <Redirect to={newPath} />;
+};
+
+const zerodhaOauthRedirect = () => {
+  alert('hello');
+  let urlParams = new URLSearchParams(window.location.search);
+  let newPath = '/app/oauth/zerodha?' + urlParams;
   return <Redirect to={newPath} />;
 };
 
@@ -429,6 +440,19 @@ const App = () => {
                 exact
                 path="/oauth/td"
                 render={() => tdAmeritradeOauthRedirect()}
+              />
+            )}
+            {loggedIn && (
+              <Route
+                path={prefixPath('/oauth/zerodha')}
+                component={ZerodhaOauthPage}
+              />
+            )}
+            {loggedIn && (
+              <Route
+                exact
+                path="/oauth/zerodha"
+                render={() => zerodhaOauthRedirect()}
               />
             )}
             {loggedIn && (
