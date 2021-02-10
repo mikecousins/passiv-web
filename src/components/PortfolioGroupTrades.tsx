52e56f572c7817bb027b7aa10d70c68f0734a9c7
Nick McCullum <nicholasmccullum@gmail.com>
2021-02-10 12:00:08 -0400

    imported new ZerodhaRebalanceWidget component and used the ternary operator to show it exclusively for Zerodha accounts

--- src/components/PortfolioGroupTrades.tsx
+++ src/components/PortfolioGroupTrades.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import RebalanceWidget from './RebalanceWidget';
+import ZerodhaRebalanceWidget from './ZerodhaRebalanceWidget';
 import { H2, H3, Title, P, BulletUL, A } from '../styled/GlobalElements';
 import {
   TradesContainer,
@@ -209,13 +210,24 @@ export const PortfolioGroupTrades = ({
           <H2>Trades</H2>
           {sellsListRender}
           {buysListRender}
-          <RebalanceWidget
-            trades={trades}
-            groupId={groupId}
-            onClose={onClose}
-            tradesTrigger={() => triggerTradesSubmitted()}
-            tradesUntrigger={() => untriggerTradesSubmitted()}
-          />
+          {accounts[0].institution_name === 'Zerodha' ? (
+            <ZerodhaRebalanceWidget
+              trades={trades}
+              groupId={groupId}
+              onClose={onClose}
+              tradesTrigger={() => triggerTradesSubmitted()}
+              tradesUntrigger={() => untriggerTradesSubmitted()}
+            />
+          ) : (
+            <RebalanceWidget
+              trades={trades}
+              groupId={groupId}
+              onClose={onClose}
+              tradesTrigger={() => triggerTradesSubmitted()}
+              tradesUntrigger={() => untriggerTradesSubmitted()}
+            />
+          )}
+
           <TradesExplanation
             settings={settings}
             accounts={groupAccounts}
