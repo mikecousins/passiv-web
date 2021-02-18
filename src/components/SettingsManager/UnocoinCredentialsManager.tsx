87aff97f44bb2a64b0324d01b8ae097060b841d7
Nick McCullum <nicholasmccullum@gmail.com>
2021-02-18 14:39:37 -0400

    removed edit button on Unocoin auth page

--- src/components/SettingsManager/UnocoinCredentialsManager.tsx
+++ src/components/SettingsManager/UnocoinCredentialsManager.tsx
@@ -3,12 +3,12 @@ import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { selectSettings, selectIsDemo } from '../../selectors';
-import { useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { InputNonFormik } from '../../styled/Form';
 import { H2, Edit, A, OptionsTitle, P } from '../../styled/GlobalElements';
 import { Button } from '../../styled/Button';
 import ShadowBox from '../../styled/ShadowBox';
+import { postData } from '../../api';
 
 const InputContainer = styled.div`
   padding-top: 10px;
@@ -52,10 +52,8 @@ const UnocoinCredentialsManager = () => {
 
   const handleSubmit = () => {
     let token_string = generateTokenString();
-    history.push(`/app/oauth/Unocoin?code=${token_string}`);
+    postData('/brokerages/authComplete/', { token: token_string });
   };
-
-  const history = useHistory();
 
   return (
     <ShadowBox>
@@ -64,40 +62,27 @@ const UnocoinCredentialsManager = () => {
         To connect your Unocoin account to Passiv, you'll need to generate a new
         Unocoin API key and enter your credentials below.
       </P>
-      {editingAPIKey ? (
-        <InputContainer>
-          <MiniInputNonFormik
-            value={APIKey === null ? '' : APIKey}
-            onChange={(e) => setAPIKey(e.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                finishEditingAPIKey();
-              }
-            }}
-            placeholder={'API Key'}
-          />
-          <Button onClick={finishEditingAPIKey}>Done</Button>
-        </InputContainer>
-      ) : (
-        <InputContainer>
-          <OptionsTitle>API Key:</OptionsTitle>{' '}
-          {APIKey === null ? '[no name set]' : APIKey}
-          <Edit
-            onClick={() => !isDemo && startEditingAPIKey()}
-            disabled={isDemo}
-          >
-            <FontAwesomeIcon icon={faPen} />
-            Edit
-          </Edit>
-        </InputContainer>
-      )}
+
+      <InputContainer>
+        <MiniInputNonFormik
+          value={APIKey === null ? '' : APIKey}
+          onChange={(e) => setAPIKey(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              finishEditingAPIKey();
+            }
+          }}
+          placeholder={'API Key'}
+        />
+        <Button onClick={handleSubmit}>Done</Button>
+      </InputContainer>
+
       <P>
         If you're stuck, read our{' '}
         <A href="#">
           tutorial on how to connect your Unocoin account to Passiv.
         </A>
       </P>
-      <Button onClick={handleSubmit}>Done</Button>
     </ShadowBox>
   );
 };
