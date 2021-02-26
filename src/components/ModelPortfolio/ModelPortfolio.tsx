ee46195fe4ac50ba75ad7ffa11f01bde0bf56dfe
ShayanAbedi <abedi.shayan@unb.ca>
2021-02-26 10:47:50 -0400

    add coming soon tag for asset class and share model

--- src/components/ModelPortfolio/ModelPortfolio.tsx
+++ src/components/ModelPortfolio/ModelPortfolio.tsx
@@ -23,7 +23,9 @@ import {
   faClipboard,
   faClipboardCheck,
   faExclamationTriangle,
+  faHourglass,
   faSpinner,
+  faStopwatch,
   faToggleOff,
   faToggleOn,
   faTrashAlt,
@@ -31,7 +33,7 @@ import {
 import { toast } from 'react-toastify';
 import ShadowBox from '../../styled/ShadowBox';
 import Grid from '../../styled/Grid';
-import { H3 } from '../../styled/GlobalElements';
+import { H3, P } from '../../styled/GlobalElements';
 import { StateText, ToggleButton } from '../../styled/ToggleButton';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import {
@@ -74,7 +76,7 @@ export const BackButton = styled.div`
 `;
 
 const ToggleBtn = styled(FontAwesomeIcon)`
-  color: var(--brand-blue);
+  color: grey;
   font-size: 30px;
 `;
 
@@ -129,6 +131,32 @@ const ShareLinkContainer = styled.div`
   justify-content: space-between;
   margin-top: 10px;
   margin-left: 5px;
+`;
+
+const ComingSoonBanner = styled.span`
+  border: 2px solid orange;
+  border-radius: 10px;
+  padding: 12px 20px;
+  font-weight: 900;
+  font-size: 32px;
+  display: inline-block;
+  position: relative;
+  top: 120px;
+  right: 50px;
+  left: 10px;
+  /* opacity: 0.8; */
+  color: orange;
+  /* margin: 0 50px 10px 50px; */
+  rotate: 30deg;
+  background-color: rgba(219, 214, 214, 0.2);
+  z-index: 3;
+`;
+
+const ComingSoonTag = styled.div`
+  font-weight: 900;
+  font-size: 30px;
+  color: rgba(255, 165, 0, 0.7);
+  margin-bottom: 10px;
 `;
 
 const ModelPortfolio = () => {
@@ -286,6 +314,11 @@ const ModelPortfolio = () => {
                   sharedModel={sharedModel}
                 />
                 <div>
+                  {/* <ComingSoonBanner>Coming Soon</ComingSoonBanner> */}
+                  <ComingSoonTag>
+                    <FontAwesomeIcon icon={faStopwatch} size="sm" /> Coming Soon
+                    ...
+                  </ComingSoonTag>
                   <div>
                     <H3>Model Type</H3>
                     <ToggleModelTypeBtn
