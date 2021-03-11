7e7613729313a7feb0a18a4e3f7288a43660a45c
Nick McCullum <nicholasmccullum@gmail.com>
2021-03-11 13:27:53 -0400

    finalized the formatting of LoginNotificationEmail

--- src/components/SettingsManager/LoginNotificationSettings.tsx
+++ src/components/SettingsManager/LoginNotificationSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,60 +7,18 @@ import { loadSettings } from '../../actions';
 import { putData } from '../../api';
 import { ToggleButton, StateText } from '../../styled/ToggleButton';
 import { OptionsTitle, DisabledBox } from '../../styled/GlobalElements';
-import { Settings } from '../../types/settings';
 
 const LoginNotificationSettings = () => {
   const settings = useSelector(selectSettings);
   const dispatch = useDispatch();
-  const [cashThreshold, setCashThreshold] = useState('');
-
-  useEffect(() => {
-    if (settings) {
-      setCashThreshold(
-        settings && parseFloat(settings.cash_email_threshold).toString(),
-      );
-    }
-  }, [settings]);
 
   const updateNotification = () => {
     if (!settings) {
       return;
     }
-    let newSettings: Settings = { ...settings };
-    newSettings.receive_cash_notifications = !settings.receive_cash_notifications;
-    putData('/api/v1/settings/', newSettings)
-      .then(() => {
-        dispatch(loadSettings());
-      })
-      .catch(() => {
-        dispatch(loadSettings());
-      });
-  };
 
-  const finishEditingThreshold = () => {
-    if (!settings) {
-      return;
-    }
-
-    let newSettings: Settings = { ...settings };
-
-    newSettings.cash_email_threshold = cashThreshold;
-
-    putData('/api/v1/settings/', newSettings)
-      .then(() => {
-        dispatch(loadSettings());
-      })
-      .catch(() => {
-        dispatch(loadSettings());
-      });
-  };
-
-  const updateSummaryThresholdNotification = () => {
-    if (!settings) {
-      return;
-    }
-    let newSettings: Settings = { ...settings };
-    newSettings.apply_cash_email_threshold_to_summary_email = !settings.apply_cash_email_threshold_to_summary_email;
+    let newSettings = { ...settings };
+    newSettings.login_emails_enabled = !settings.login_emails_enabled;
     putData('/api/v1/settings/', newSettings)
       .then(() => {
         dispatch(loadSettings());
@@ -74,33 +32,27 @@ const LoginNotificationSettings = () => {
     return null;
   }
 
-  let contents = (
+  return (
     <React.Fragment>
       <OptionsTitle>Login Notifications:</OptionsTitle>
-      {settings.receive_cash_notifications ? (
-        <React.Fragment>
-          <ToggleButton onClick={updateNotification}>
-            <React.Fragment>
-              <FontAwesomeIcon icon={faToggleOn} />
-              <StateText>on</StateText>
-            </React.Fragment>
-          </ToggleButton>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <ToggleButton onClick={updateNotification}>
+      <ToggleButton onClick={updateNotification}>
+        {settings.login_emails_enabled ? (
+          <React.Fragment>
+            <FontAwesomeIcon icon={faToggleOn} />
+            <StateText>on</StateText>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
             <FontAwesomeIcon icon={faToggleOff} />
             <StateText>off</StateText>
-          </ToggleButton>
-        </React.Fragment>
-      )}
+          </React.Fragment>
+        )}
+      </ToggleButton>
       <DisabledBox>
-        Receive an email notification whenever you log into your Passiv account.
+        Receive email notifications whenever you log into your Passiv account.
       </DisabledBox>
     </React.Fragment>
   );
-
-  return contents;
 };
 
 export default LoginNotificationSettings;
