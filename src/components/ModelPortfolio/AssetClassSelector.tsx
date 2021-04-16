2bc04928d51b4026cbca760f028ad256cf8aed51
Shayan <abedi.shayan@unb.ca>
2021-04-16 11:26:51 -0300

    remove redirect

--- src/components/ModelPortfolio/AssetClassSelector.tsx
+++ src/components/ModelPortfolio/AssetClassSelector.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
 import {
   Combobox,
