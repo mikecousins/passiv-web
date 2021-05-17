f241cb4e3a81bc17bf6e3d6991424300b4f7c96c
Shayan <abedi.shayan@unb.ca>
2021-05-17 11:34:20 -0300

    fix: issue with priorties get nuked when automatic refresh happens

--- src/components/ModelPortfolio/Prioritization/index.tsx
+++ src/components/ModelPortfolio/Prioritization/index.tsx
@@ -82,24 +82,29 @@ const Prioritization = ({ onSettingsPage }: Props) => {
   const assetClassTradePriorities = useSelector(
     selectCurrentGroupAssetClassTradePriorities,
   );
-
   const [assetClassPriorities, setAssetClassPriorities] = useState<
     AssetClassPriorities[]
-  >([]);
+  >(assetClassTradePriorities.tradePriorities);
 
   const [loading, setLoading] = useState(true);
-  const [editing, setEditing] = useState(!onSettingsPage);
+  const [editing, setEditing] = useState(false);
   const [changed, setChanged] = useState({ symbolId: '', accountId: '' });
-  const [needToConfirm, setNeedToConfirm] = useState<string[]>([]);
-  const [newAssets, setNewAssets] = useState<string[]>([]);
+  const [needToConfirm, setNeedToConfirm] = useState<string[]>(
+    assetClassTradePriorities.assetClassIds,
+  );
+  const [newAssets, setNewAssets] = useState<string[]>(
+    assetClassTradePriorities.newSecurities,
+  );
 
   useEffect(() => {
-    setLoading(currentGroupInfoLoading);
-    setAssetClassPriorities(assetClassTradePriorities.tradePriorities);
-    setNeedToConfirm(assetClassTradePriorities.assetClassIds);
-    setNewAssets(assetClassTradePriorities.newSecurities);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentGroupInfoLoading]);
+    if (editing === false) {
+      setLoading(currentGroupInfoLoading);
+      setAssetClassPriorities(assetClassTradePriorities.tradePriorities);
+      setNeedToConfirm(assetClassTradePriorities.assetClassIds);
+      setNewAssets(assetClassTradePriorities.newSecurities);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }
+  }, [assetClassTradePriorities]);
 
   // this function handles the up and down arrows which changes the priority for an asset class
   const handleUpDownBtn = (
@@ -116,7 +121,7 @@ const Prioritization = ({ onSettingsPage }: Props) => {
     setTimeout(() => {
       setChanged({ symbolId: '', accountId: '' });
     }, 300);
-    if (onSettingsPage && !editing) {
+    if (!editing) {
       setEditing(true);
     }
 
