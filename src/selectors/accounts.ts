619f29c2324013a2165f36334cc34edf4ca974c6
Shayan <abedi.shayan@unb.ca>
2021-05-17 12:55:44 -0300

    fix: issue with `cash` and `total value` not being displayed on account page

--- src/selectors/accounts.ts
+++ src/selectors/accounts.ts
@@ -62,8 +62,8 @@ export const selectCurrentAccountId = createSelector<
   string | null
 >(selectPathname, (pathname) => {
   let accountId = null;
-  if (pathname && pathname.split('/').length >= 6) {
-    accountId = pathname.split('/')[5];
+  if (pathname && pathname.split('/').length >= 5) {
+    accountId = pathname.split('/')[4];
   }
   return accountId;
 });
@@ -82,6 +82,7 @@ export const selectCurrentAccountBalances = createSelector<
     allBalances[accountId].data
   ) {
     balances = allBalances[accountId].data;
+    console.log(balances);
   }
   return balances;
 });
