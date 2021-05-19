82fca4ecd4331b7106ed95e9ab0352bd2fcf4806
Shayan <abedi.shayan@unb.ca>
2021-05-19 13:03:13 -0300

    create a selector `hasOnlyCryptoAccounts` and use it to hide or show the course notification

--- src/selectors/accounts.ts
+++ src/selectors/accounts.ts
@@ -133,3 +133,18 @@ export const selectHiddenAccounts = createSelector<AppState, any[], any[]>(
     return hiddenAccounts;
   },
 );
+
+export const selectHasOnlyCryptoAccounts = createSelector(
+  selectAccounts,
+  (accounts) => {
+    let hasOnlyCrypto = false;
+    const numberOfCryptoAccounts = accounts.filter(
+      (account) => account.meta.type === 'Crypto',
+    ).length;
+
+    if (numberOfCryptoAccounts === accounts.length) {
+      hasOnlyCrypto = true;
+    }
+    return hasOnlyCrypto;
+  },
+);
