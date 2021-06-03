f077d3d8f305d41868912182bb90c5db2cde7b0a
Shayan <shayanabedi.dev@gmail.com>
2021-06-03 15:01:40 -0300

    feat: add a checkbox for excluding/unexcluding multiple assets

--- src/components/PortfolioGroupSettings/ExcludedAssets.tsx
+++ src/components/PortfolioGroupSettings/ExcludedAssets.tsx
@@ -8,7 +8,7 @@ import {
   faCaretUp,
   faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
-import { deleteData, postData } from '../../api';
+import { postData } from '../../api';
 import { loadGroup } from '../../actions';
 import {
   selectCurrentGroupId,
@@ -33,6 +33,21 @@ const Container = styled.div`
     margin-bottom: 37px;
   }
 `;
+
+const CheckAll = styled(Grid)`
+  border-bottom: 0.5px solid #bfb6b6;
+  width: 200px;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+  align-items: center;
+  margin-left: 20px;
+  align-items: center;
+  @media (max-width: 900px) {
+    display: grid;
+    grid-gap: 20px;
+  }
+`;
+
 const Positions = styled(Grid)`
   margin-bottom: 20px;
   align-items: center;
@@ -99,6 +114,10 @@ const ExcludedAssets = () => {
   const setupComplete = useSelector(selectCurrentGroupSetupComplete);
   const [showAssets, setShowAssets] = useState(false);
 
+  const allExcluded = positionsNotInTargetOrExcluded.every(
+    (target) => target.excluded,
+  );
+
   useEffect(() => {
     setLoading(groupInfoLoading);
   }, [groupInfoLoading]);
@@ -112,26 +131,42 @@ const ExcludedAssets = () => {
     },
     0,
   );
-
-  const handleCheckBoxClick = (position: any) => {
+  const listOfAllAssetIds = positionsNotInTargetOrExcluded.map(
+    (target) => target.symbol.id,
+  );
+  const handleCheckBoxClick = (position: any, checkAll: boolean) => {
     setLoading(true);
-    const positionId = position.symbol.id;
-    if (currentGroupModelType === 1) {
-      let excluded = positionsNotInTargetOrExcluded
+    let excluded = [];
+    // if check all clicked
+    if (checkAll) {
+      // if not all excluded, means we want to exclude all assets
+      if (!allExcluded) {
+        excluded = listOfAllAssetIds;
+      }
+    }
+    // else, the individual checkbox clicked
+    else {
+      const positionId = position.symbol.id;
+      excluded = positionsNotInTargetOrExcluded
         .map((position) => {
           if (position.excluded) {
             return position.symbol.id;
           }
           return false;
         })
-        .filter((id) => typeof id === 'string');
+        .filter((id) => typeof id === 'string'); // to remove undefined values
 
+      // if position is already excluded, it means we want to unexclude it
       if (position.excluded) {
         excluded = excluded.filter((id) => id !== positionId);
-      } else {
+      }
+      // otherwise, add it to excluded list
+      else {
         excluded.push(positionId);
       }
-
+    }
+    /* if an asset class based model */
+    if (currentGroupModelType === 1) {
       postData(
         `/api/v1/portfolioGroups/${groupId}/assetClassExcludeAssets`,
         excluded,
@@ -144,29 +179,14 @@ const ExcludedAssets = () => {
           setLoading(false);
         });
     } else {
-      if (position.excluded) {
-        deleteData(
-          `/api/v1/portfolioGroups/${groupId}/excludedassets/${positionId}`,
-        )
-          .then(() => {
-            dispatch(loadGroup({ ids: [groupId] }));
-          })
-          .catch(() => {
-            toast.error('Failed to unexclude the asset. Please try again.');
-            setLoading(false);
-          });
-      } else {
-        postData(`/api/v1/portfolioGroups/${groupId}/excludedassets/`, {
-          symbol: positionId,
+      postData(`/api/v1/portfolioGroups/${groupId}/excludedassets/`, excluded)
+        .then(() => {
+          dispatch(loadGroup({ ids: [groupId] }));
         })
-          .then(() => {
-            dispatch(loadGroup({ ids: [groupId] }));
-          })
-          .catch(() => {
-            toast.error('Failed to exclude the asset. Please try again.');
-            setLoading(false);
-          });
-      }
+        .catch(() => {
+          toast.error('Failed to exclude the asset. Please try again.');
+          setLoading(false);
+        });
     }
   };
 
@@ -201,6 +221,22 @@ const ExcludedAssets = () => {
         positionsNotInTargetOrExcluded.length > 0 ? (
           showAssets && (
             <>
+              <CheckAll columns="10px 180px">
+                <CheckBox>
+                  <label className="container">
+                    <input
+                      type="checkbox"
+                      checked={allExcluded}
+                      onChange={() => handleCheckBoxClick(0, true)}
+                    />
+                    <span className="checkmark"></span>
+                  </label>
+                </CheckBox>
+                <Symbol disabled={false}>
+                  {allExcluded ? 'Unexclude all' : 'Exclude all'}
+                </Symbol>
+              </CheckAll>
+
               {positionsNotInTargetOrExcluded.map((position) => {
                 return (
                   <Positions key={position.symbol.id} columns="10px 180px auto">
@@ -209,7 +245,7 @@ const ExcludedAssets = () => {
                         <input
                           type="checkbox"
                           checked={position.excluded}
-                          onChange={() => handleCheckBoxClick(position)}
+                          onChange={() => handleCheckBoxClick(position, false)}
                           disabled={!position.quotable}
                         />
                         <span className="checkmark"></span>
